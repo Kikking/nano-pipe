21e49784a86555160b95a20f728c2312d5df6c66
Kikking <kikkingkeenan@gmail.com>
2023-06-24 16:41:53 +0200

    rm cd LongQC

--- LongQC.sh
+++ LongQC.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cd LongQC
+
 
 for i in ~/mount/preqc; do 
     echo $i;
-    python longQC.py sampleqc -x ont-rapid -o postqc/"QC_"+="$i"  $i;
+    python longQC/longQC.py sampleqc -x ont-rapid -o postqc/"QC_"+="$i"  $i;
 done
