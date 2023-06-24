0781545b3aa7cc6d242719102dbcb4327b984ebf
Kikking <kikkingkeenan@gmail.com>
2023-06-24 17:43:45 +0200

    Added Comments

--- autopull.sh
+++ autopull.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+#A way to auto pull from the nano-pipe repo and maintain position in current working directory
+
 x= $PWD
 cd
 cd nano-pipe
 git pull
 cd $x
+
+#save as autopull=`sudo bash ~/nano-pipe/autopull.sh`
+# call : echo $autopull
