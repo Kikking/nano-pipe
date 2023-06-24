994cc9332747e458c2c84f376264b7983d2c941a
Kikking <kikkingkeenan@gmail.com>
2023-06-24 18:47:26 +0200

    removed all ;

--- LongQC.sh
+++ LongQC.sh
@@ -4,6 +4,6 @@
 
 for i in ~/mount/preqc/*
 do 
-    echo "$i";
-    python ~/LongQC/longQC.py sampleqc -x ont-rapid -o QC_i  "$i";
+    echo "$i"
+    python ~/LongQC/longQC.py sampleqc -x ont-rapid -o QC_i  "$i"
 done
