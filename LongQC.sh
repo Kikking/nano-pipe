81b3b6c01745067a1e18687afd98835e3aae1f3a
Kikking <kikkingkeenan@gmail.com>
2023-06-24 16:48:25 +0200

    longqc.py file ref

--- LongQC.sh
+++ LongQC.sh
@@ -4,5 +4,5 @@
 
 for i in ~/mount/preqc; do 
     echo $i;
-    python longQC/longQC.py sampleqc -x ont-rapid -o postqc/"QC_"+="$i"  $i;
+    python ~/LongQC/longQC.py sampleqc -x ont-rapid -o postqc/"QC_"+="$i"  $i;
 done
