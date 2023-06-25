87f95de08e882112da5e5bf6b40ec06234deb928
Kikking <kikkingkeenan@gmail.com>
2023-06-25 22:23:19 +0200

    COMBINED FILE FIND AND LONGQC SCRIPT

--- LongQC.sh
+++ LongQC.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+CELL_LINE=$1
 
+mkdir ~/mount/"$CELL_LINE"_QC
+find $HOME/mount/nanopore_data/ -type f -iname  *"$CELL_LINE"*"DNA"*".fastq.gz" -exec cp {} ~/mount/"$CELL_LINE"_QC/ \;
 
-for i in ~/mount/preqc/*
-do 
+for i in ~/mount/"$CELL_LINE"_QC/*
+do
     echo "$i"
     python ~/LongQC/longQC.py sampleqc -x ont-ligation -o $HOME/mount/postqc/QC_"${i##*/}"  "$i"
 done
