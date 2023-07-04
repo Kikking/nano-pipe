b4d369d952f10c9eaafa70664d65dabfc55e2e85
Kikking <kikkingkeenan@gmail.com>
2023-07-04 14:36:18 +0200

    "${i##*/}"

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -4,4 +4,4 @@ SIRV_REF=$HOME/mount/refiles/lrgasp_grch38_sirvs.fasta.gz
 SIRV_ANNO=$HOME/mount/refiles/lrgasp_gencode_v38_sirvs.gtf.gz
 SAMPLE=$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
 
-minimap2 -ax splice $SIRV_REF $SAMPLE > map_"$SAMPLE".sam
+minimap2 -ax splice $SIRV_REF $SAMPLE > map_"${SAMPLE##*/}".sam
