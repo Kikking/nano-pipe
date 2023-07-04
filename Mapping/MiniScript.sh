8a10707d62a643158c25bee75f642d15136b1f70
Kikking <kikkingkeenan@gmail.com>
2023-07-04 18:16:39 +0200

    Mapping test

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -2,6 +2,9 @@
 
 SIRV_REF=$HOME/lrgasp_grch38_sirvs.fasta.gz
 SIRV_ANNO=$HOME/lrgasp_gencode_v38_sirvs.gtf.gz
-SAMPLE=$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
+SAMPLE=$1
+#$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
 
-./minimap2 -ax splice $SIRV_REF $SAMPLE > map_"${SAMPLE##*/}".sam
+cd
+cd minimap2
+./minimap2 -ax splice $SIRV_REF $SAMPLE > $HOME/minitest/map_"${SAMPLE##*/}".sam
