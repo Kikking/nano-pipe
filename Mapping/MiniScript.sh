13d46ac9072a09a14415a5204370f040da20694b
Kikking <kikkingkeenan@gmail.com>
2023-07-12 11:27:39 +0200

    miniscript loop

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -2,9 +2,10 @@
 
 SIRV_REF=$HOME/lrgasp_grch38_sirvs.fasta.gz
 SIRV_ANNO=$HOME/lrgasp_gencode_v38_sirvs.gtf.gz
-SAMPLE=$1
+TARGET=$1
 #$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
-
 cd
 cd minimap2
-./minimap2 -ax splice $SIRV_REF $SAMPLE > $HOME/minitest/map_"${SAMPLE##*/}".sam
+for SAMPLE in $TARGET/*
+./minimap2 -ax splice $SIRV_REF $SAMPLE | samtools view -b | samtools sort > $HOME/mount/minidata/map_"${SAMPLE##*/}".bam
+ 
