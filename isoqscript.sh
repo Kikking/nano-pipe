d892fb4aa9d33f52c7bcabc8e7c28b33c7f5c9ac
Kikking <kikkingkeenan@gmail.com>
2023-07-18 16:37:31 +0200

    .bai -> .bam

--- isoqscript.sh
+++ isoqscript.sh
@@ -3,6 +3,6 @@
 SIRV_REF=$HOME/mount2/ref_data/lrgasp_grch38_sirvs.fasta.gz
 SIRV_ANNO=$HOME/mount2/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
 TARGET=$1
-samtools index $TARGET > $HOME/mount2/minidata_index/"${TARGET##*/}".bai
- isoquant.py --reference $SIRV_REF  --genedb $SIRV_ANNO --bam $HOME/mount2/minidata_index/"${TARGET##*/}".bai --data_type nanopore -o $HOME/mount2/isoqdata
+samtools index $TARGET > $HOME/mount2/minidata_index/"${TARGET##*/}".bam
+ isoquant.py --reference $SIRV_REF  --genedb $SIRV_ANNO --bam $HOME/mount2/minidata_index/"${TARGET##*/}".bam --data_type nanopore -o $HOME/mount2/isoqdata
 
