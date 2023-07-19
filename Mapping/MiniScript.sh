397b58ecbcbeb35d487ed6f83a96448e536e337d
Kikking <kikkingkeenan@gmail.com>
2023-07-19 11:51:53 +0200

    edit minimap2 to .sam output

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SIRV_REF=$HOME/lrgasp_grch38_sirvs.fasta.gz
-SIRV_ANNO=$HOME/lrgasp_gencode_v38_sirvs.gtf.gz
+SIRV_REF=$HOME/mount2/ref_data/lrgasp_grch38_sirvs.mmi
+SIRV_ANNO=$HOME/mount2/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
 TARGET=$1
 #$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
 cd
 cd minimap2
 for SAMPLE in $TARGET/*
 do
-./minimap2 -ax splice $SIRV_REF $SAMPLE | samtools view -b | samtools sort > $HOME/mount/minidata/map_"${SAMPLE##*/}".bam
+./minimap2 -ax splice $SIRV_REF $SAMPLE > $HOME/mount2/minidata_sam/map_"${SAMPLE##*/}".sam
  done
