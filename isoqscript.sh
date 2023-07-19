6d1e4ebf0e7849bdd0a7954f90740e417b3fd8a2
Kikking <kikkingkeenan@gmail.com>
2023-07-19 17:11:37 +0200

    fixed genedb error

--- isoqscript.sh
+++ isoqscript.sh
@@ -4,5 +4,5 @@ SIRV_REF=$HOME/mount2/ref_data/lrgasp_grch38_sirvs.fasta.gz
 SIRV_ANNO=$HOME/mount2/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
 TARGET=$1
 #samtools index $TARGET > $HOME/mount2/minidata_index/"${TARGET##*/}".bam
-isoquant.py --reference $SIRV_REF  --complete_genedb $SIRV_ANNO --bam $TARGET --data_type nanopore -o $HOME/mount2/isoqdata 
+isoquant.py --reference $SIRV_REF  --genedb $SIRV_ANNO --complete_genedb --bam $TARGET --data_type nanopore -o $HOME/mount2/isoqdata 
 
