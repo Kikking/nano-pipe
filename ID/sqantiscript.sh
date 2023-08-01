917c751db536c349ca6ebe17fcb30111f34e9af4
Kikking <kikkingkeenan@gmail.com>
2023-08-01 16:39:00 +0200

    changed to unzipped files

--- ID/sqantiscript.sh
+++ ID/sqantiscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SIRV_REF=$HOME/mount/ref_data/lrgasp_grch38_sirvs.fasta.gz
-SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
+SIRV_REF=$HOME/mount/ref_data/lrgasp_grch38_sirvs.fasta
+SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf
 TARGET=$1
 
 python sqanti3_qc.py $TARGET $SIRV_ANNO $SIRV_REF \
