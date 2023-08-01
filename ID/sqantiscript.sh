1107f5278aa14ceea7ad9555cbd37d6549495cf0
Kikking <kikkingkeenan@gmail.com>
2023-08-01 15:54:46 +0200

    init of sqanti3 script

---
+++ ID/sqantiscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SIRV_REF=$HOME/mount/ref_data/lrgasp_grch38_sirvs.fasta.gz
+SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
+TARGET=$1
+
+python sqanti3_qc.py $TARGET $SIRV_ANNO $SIRV_REF \
+-o ST_SQ_A_r2r1 \
+-d mount/sqant_output \
+--cpus 4 --report both                     
