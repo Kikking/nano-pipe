6b6a7d47f42c4fc88c4b5753db5318535d7674f0
Kikking <kikkingkeenan@gmail.com>
2023-08-07 17:57:38 +0200

    changed mount2 to mount

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-SIRV_REF=$HOME/mount2/ref_data/lrgasp_grch38_sirvs.mmi
-SIRV_ANNO=$HOME/mount2/ref_data/lrgasp_gencode_v38_sirvs.gtf.gz
+SIRV_REF=$HOME/mount/ref_data/lrgasp_grch38_sirvs.mmi
+SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf
 TARGET=$1
-#$HOME/mount/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
+#$HOME/mount2/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
 cd
 cd minimap2
 for SAMPLE in $TARGET/*
 do
 ./minimap2 -ax splice $SIRV_REF $SAMPLE > $HOME/mount2/minidata_sam/map_"${SAMPLE##*/}".sam
  done
+
+ #minimap2 -ax splice $HOME/mount2/ref_data/lrgasp_grch38_sirvs.mmi $HOME/mount2/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz > $HOME/mount2/minidata_sam/A_d_r2r1.sam
