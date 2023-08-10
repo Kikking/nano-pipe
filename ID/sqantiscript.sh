991ae87452d0598afa47483af193a14dfe19efde
Kikking <kikkingkeenan@gmail.com>
2023-08-10 17:50:16 +0200

    Version control

--- ID/sqantiscript.sh
+++ ID/sqantiscript.sh
@@ -7,3 +7,9 @@ python sqanti3_qc.py $TARGET $SIRV_ANNO $SIRV_REF \
 -o ST_SQ_A_r2r1 \
 -d mount/sqant_output \
 --cpus 4 --report both                     
+
+
+python sqanti3_qc.py $HOME/mount/test2.gtf $HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf $HOME/mount/ref_data/lrgasp_grch38_sirvs.fasta \
+-o ST_SQ_A_r2r1 \
+-d mount/sqant_output \
+--cpus 8 --report pdf
--- Mapping/syndown.sh
+++ Mapping/syndown.sh
@@ -12,3 +12,5 @@ cd mount2
 cd ref_data
 synapse get syn25683364 #lrgasp_grch38_sirvs.fasta.gz
 synapse get syn25683628 #lrgasp_gencode_v38_sirvs.gtf.gz
+
+~/mount2/minidata/map_SGNex_A549_cDNAStranded_replicate3_run3.fastq.gz.bam
