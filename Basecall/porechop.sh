594107a252aa2fd8f9dbdfa54134183de105cda3
Kikking <kikkingkeenan@gmail.com>
2023-08-30 19:00:11 +0200

    loop fix

--- Basecall/porechop.sh
+++ Basecall/porechop.sh
@@ -3,5 +3,5 @@
 TARGET=$1
 
 for SAMPLE in $TARGET
-porechop_abi -abi -i ~/mount/raw_SGNex/SGNex_A549_cDNAStranded_replicate5_run2.fastq.gz -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
+porechop_abi -abi -i $SAMPLE -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
 done
