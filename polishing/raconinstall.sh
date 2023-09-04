5b2fe72191193898795b56856e38c592c2e0e71c
Kikking <kikkingkeenan@gmail.com>
2023-09-04 20:24:37 +0200

    racon setup

---
+++ polishing/raconinstall.sh
@@ -0,0 +1,13 @@
+#!/bash/bin
+
+sudo apt update
+sudo apt install build-essential
+sudo apt-get install libz-dev
+conda install -c anaconda make
+
+git clone --recursive https://github.com/lbcb-sci/racon.git racon
+cd racon
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
