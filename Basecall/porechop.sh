5a92f5e782c0bac82aad8a6c8f37abb4b28dda3a
Kikking <kikkingkeenan@gmail.com>
2023-09-11 10:56:37 +0200

    porechop fix

--- Basecall/porechop.sh
+++ Basecall/porechop.sh
@@ -2,7 +2,7 @@
 
 TARGET=$1
 
-for SAMPLE in $TARGET
+for SAMPLE in $TARGET/*
 do
-porechop_abi -abi -i $SAMPLE -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
+/porechop_abi -abi -i $SAMPLE -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
 done
