15a98ae75a8a0a4310eec2c2bfdc67fcafef3c6f
Kikking <kikkingkeenan@gmail.com>
2023-09-11 11:09:48 +0200

    fix command call-

--- Basecall/porechop.sh
+++ Basecall/porechop.sh
@@ -4,5 +4,5 @@ TARGET=$1
 
 for SAMPLE in $TARGET/*
 do
-/porechop_abi -abi -i $SAMPLE -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
+porechop_abi -abi -i $SAMPLE -o $HOME/mount/trim/trim_"${SAMPLE##*/}"
 done
