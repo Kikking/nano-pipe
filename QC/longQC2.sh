be3604597cecbcb7119fd05235232957a510bd2f
Kikking <kikkingkeenan@gmail.com>
2023-09-12 20:49:34 +0200

    visualize changing from one file to next

--- QC/longQC2.sh
+++ QC/longQC2.sh
@@ -5,6 +5,8 @@ ONT_TYPE=${2:-ont-ligation}
 
 for i in $TARGET/*
 do
+    echo "#########################################################################"
     echo "$i"
+    echo "#########################################################################"
     python ~/LongQC/longQC.py sampleqc -x "$ONT_TYPE" -o $HOME/mnt/d/postqc_IFN/QC_"${i##*/}"_"$ONT_TYPE"  "$i"
 done
