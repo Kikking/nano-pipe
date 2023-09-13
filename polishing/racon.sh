e6336cf09ec9dba76505091b5361de2191014247
Kikking <kikkingkeenan@gmail.com>
2023-09-13 16:16:43 +0200

    racon test paths

--- polishing/racon.sh
+++ polishing/racon.sh
@@ -1,7 +1,7 @@
 #!/bash/bin
 
-PORECHOPPED=/mnt/d/raw_IFN/
-MAPPED=/mnt/d/minidata_sam
-REFERENCE=
+PORECHOPPED=/home/kikking/pore_test_swap
+MAPPED=/mnt/d/minidata_bam/sorted_A549_IFNB_nanopore.bam
+REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.mmi
 
-racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > rac_"${PORECHOPPED##*/}".fasta
+racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/d/rac/IFN/rac_"${PORECHOPPED##*/}".fasta
