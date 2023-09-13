214a4c4277375fb461d195c0e58fdfcdacd8f7e9
Kikking <kikkingkeenan@gmail.com>
2023-09-13 16:22:58 +0200

    mmi -> fasta

--- polishing/racon.sh
+++ polishing/racon.sh
@@ -2,6 +2,6 @@
 
 PORECHOPPED=/home/kikking/pore_test_swap.fastq
 MAPPED=/mnt/d/minidata_sam/map_A549_IFNB_nanopore.fastq.gz.sam
-REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.mmi
+REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 
 racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/d/rac/IFN/rac_"${PORECHOPPED##*/}".fasta
