172104f41b0b7794d5bb13d5f336f7837b34c1bb
Kikking <kikkingkeenan@gmail.com>
2023-09-15 12:44:03 +0200

    added minimap2 var to PATH

--- Mapping/MiniScript.sh
+++ Mapping/MiniScript.sh
@@ -4,11 +4,10 @@ SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.mmi
 SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf
 TARGET=$1
 #$HOME/mount2/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz
-cd
-cd minimap2
+
 for SAMPLE in $TARGET/*
 do
-./minimap2 -ax splice $SIRV_REF $SAMPLE > /mnt/d/minidata_sam/map_"${SAMPLE##*/}".sam
+minimap2 -ax splice $SIRV_REF $SAMPLE > /mkmnt/c/Users/User/Desktop/darter/minidata_sam/map_"${SAMPLE##*/}".sam
 done
 
  #minimap2 -ax splice $HOME/mount2/ref_data/lrgasp_grch38_sirvs.mmi $HOME/mount2/A549_QC/SGNex_A549_directcDNA_replicate2_run1.fastq.gz > $HOME/mount2/minidata_sam/A_d_r2r1.sam
