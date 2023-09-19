59c1e1192a235e239bcd39003cd4573a68d56a69
Kikking <kikkingkeenan@gmail.com>
2023-09-19 17:52:40 +0200

    racon for loop

--- polishing/racon.sh
+++ polishing/racon.sh
@@ -1,7 +1,31 @@
 #!/bash/bin
 
-PORECHOPPED=/mnt/c/Users/User/Desktop/darter/testSample.fq
-MAPPED=/mnt/c/Users/User/Desktop/darter/minidata_sam/map_testSample.fq.sam
+FILE=$1 #f or d (file or directory)
+TARGET=$2
 REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 
-racon_wrapper -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${PORECHOPPED##*/}".fasta
+#For running Racon on individual files
+if [ $FILE=="f" ]
+then 
+    PORECHOPPED=/mnt/c/Users/User/Desktop/darter/"${TARGET##*/}"
+    MAPPED=/mnt/c/Users/User/Desktop/darter/minidata_sam/map_"${TARGET##*/}".sam
+    racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${TARGET##*/}".fasta
+fi
+
+#For running Racon on directories
+if [ $FILE=="d" ]
+then
+    for SAMPLE in $TARGET/*
+    do 
+    PORECHOPPED=/mnt/c/Users/User/Desktop/darter/"${SAMPLE##*/}"
+    MAPPED=/mnt/c/Users/User/Desktop/darter/minidata_sam/map_"${SAMPLE##*/}".sam
+
+    echo "#########################################################################"
+    echo  $PORECHOPPED 
+    echo  $MAPPED
+    echo "#########################################################################"
+
+    
+    racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${SAMPLE##*/}".fasta
+    done
+fi 
