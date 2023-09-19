be2c1e480adb086be19cce1a524d178d85b03381
Kikking <kikkingkeenan@gmail.com>
2023-09-19 16:15:17 +0200

    racon_wrapper

--- polishing/racon.sh
+++ polishing/racon.sh
@@ -4,4 +4,4 @@ PORECHOPPED=/mnt/c/Users/User/Desktop/darter/testSample.fq
 MAPPED=/mnt/c/Users/User/Desktop/darter/minidata_sam/map_testSample.fq.sam
 REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 
-racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${PORECHOPPED##*/}".fasta
+racon_wrapper -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${PORECHOPPED##*/}".fasta
