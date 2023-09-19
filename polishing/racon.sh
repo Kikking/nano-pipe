b7507998d6c0c60d8cf34ded411120bea76e9d48
Kikking <kikkingkeenan@gmail.com>
2023-09-19 16:07:43 +0200

    for testSample

--- polishing/racon.sh
+++ polishing/racon.sh
@@ -1,7 +1,7 @@
 #!/bash/bin
 
-PORECHOPPED=/mnt/d/raw_IFN/A549_IFNB_nanopore.fastq.gz
-MAPPED=/mnt/d/minidata_sam/map_A549_IFNB_nanopore.fastq.gz.sam
+PORECHOPPED=/mnt/c/Users/User/Desktop/darter/testSample.fq
+MAPPED=/mnt/c/Users/User/Desktop/darter/minidata_sam/map_testSample.fq.sam
 REFERENCE=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 
-racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/d/rac/IFN/rac_"${PORECHOPPED##*/}".fasta
+racon -m 8 -x -6 -g -8 -w 500 -t 8 $PORECHOPPED $MAPPED $REFERENCE > /mnt/c/Users/User/Desktop/darter/rac/rac_"${PORECHOPPED##*/}".fasta
