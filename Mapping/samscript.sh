36ceec6f7ce029e3572e0803cd714ce331e900a9
Kikking <kikkingkeenan@gmail.com>
2023-09-20 10:35:27 +0200

    rm "

--- Mapping/samscript.sh
+++ Mapping/samscript.sh
@@ -2,5 +2,5 @@
 
 SAMPLE=$1
 
-samtools view -b $SAMPLE | samtools sort > /mnt/c/Users/User/Desktop/darter/minidata_bam/"${SAMPLE%.*}".bam
-qualimap bamqc -bam /mnt/c/Users/User/Desktop/darter/minidata_bam/"${SAMPLE%.*}".bam -outdir /mnt/c/Users/User/Desktop/darter/qualimap/"quali_"${SAMPLE%.*}" -c -nw 400 -hm 3
+samtools view -b $SAMPLE | samtools sort > /mnt/c/Users/User/Desktop/darter/minidata_bam/${SAMPLE%.*}.bam
+qualimap bamqc -bam /mnt/c/Users/User/Desktop/darter/minidata_bam/${SAMPLE%.*}.bam -outdir /mnt/c/Users/User/Desktop/darter/qualimap/quali_${SAMPLE%.*} -c -nw 400 -hm 3
