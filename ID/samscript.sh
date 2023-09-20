f8e6ffeb5b6be1d462be6ae230d30132763306aa
Kikking <kikkingkeenan@gmail.com>
2023-09-20 10:31:50 +0200

    unique dir location for each file

--- ID/samscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda install -c bioconda samtools
-
-samtools view -bS /mnt/d/minidata_sam/map_A549_IFNB_nanopore.fastq.gz.sam > /mnt/d/minidata_bam/map_A549_IFNB_nanopore.bam
-
-samtools sort /mnt/d/minidata_bam/map_A549_IFNB_nanopore.bam > /mnt/d/minidata_bam/sorted_A549_IFNB_nanopore.bam
