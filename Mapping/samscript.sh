1a9f27ae3900a441136ffb9c7f69cfccf00acf42
Kikking <kikkingkeenan@gmail.com>
2023-09-22 18:22:59 +0200

    loop

--- Mapping/samscript.sh
+++ Mapping/samscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SAMPLE=$1
-
-samtools view -b $SAMPLE | samtools sort > /mnt/c/Users/User/Desktop/darter/minidata_bam/${SAMPLE%.*}.bam
-qualimap bamqc -bam /mnt/c/Users/User/Desktop/darter/minidata_bam/${SAMPLE%.*}.bam -outdir /mnt/c/Users/User/Desktop/darter/qualimap/quali_${SAMPLE%.*} -c -nw 400 -hm 3
+TARGET=$1
+#MODE={$2-both} #sam, quali or both 
+for SAMPLE in $TARGET/*
+do
+samtools view -b $SAMPLE | samtools sort > /mnt/d/minidata_bam/${SAMPLE%.*}.bam
+qualimap bamqc -bam /mnt/d/minidata_bam/${SAMPLE%.*}.bam -outdir /mnt/d/qualimap/quali_${SAMPLE%.*} -c -nw 400 -hm 3
+done
