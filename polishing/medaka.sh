007ccfe5beeab79719bbdc6703f24daf26793fa3
Kikking <kikkingkeenan@gmail.com>
2023-10-09 19:26:56 +0200

    error check

--- polishing/medaka.sh
+++ polishing/medaka.sh
@@ -5,7 +5,7 @@ DEVICE=min
 CALLER=high
 CALLERVER=g303
 
-source ${MEDAKA}  # i.e. medaka/venv/bin/activate
+#source ${MEDAKA}  # i.e. medaka/venv/bin/activate
 NPROC=4
 BASECALLS=/mnt/c/Users/User/Desktop/darter/testSample.fq
 DRAFT=/mnt/c/Users/User/Desktop/darter/rac/IFN/rac_testSample.fq.fasta
@@ -14,3 +14,10 @@ OUTDIR=/mnt/c/Users/User/Desktop/darter/med/IFN
 
 medaka_consensus -i ${BASECALLS} -d ${DRAFT} -o ${OUTDIR} -t ${NPROC} -m r941_min_high_g303
 #${PORE}_${DEVICE}_${CALLER}_${CALLERVER}
+
+if [ $? -eq 0 ]; then
+  echo "Medaka consensus completed successfully."
+else
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "Medaka encountered an error."
+fi
