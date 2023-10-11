2ec1c4f7e42faa595d0789181f3b741380977a36
Kikking <kikkingkeenan@gmail.com>
2023-10-11 17:09:38 +0200

    decreased batch size

--- polishing/medaka.sh
+++ polishing/medaka.sh
@@ -6,13 +6,13 @@ CALLER=high
 CALLERVER=g303
 
 #source ${MEDAKA}  # i.e. medaka/venv/bin/activate
-NPROC=4
+NPROC=$(nproc)
 BASECALLS=/mnt/c/Users/User/Desktop/darter/testSample.fq
 DRAFT=/mnt/c/Users/User/Desktop/darter/rac/rac_testSample.fq.fasta
 OUTDIR=/mnt/c/Users/User/Desktop/darter/med/IFN
 
 
-medaka_consensus -i ${BASECALLS} -d ${DRAFT} -o ${OUTDIR} -t ${NPROC} -m r941_min_high_g303
+medaka_consensus -i ${BASECALLS} -d ${DRAFT} -o ${OUTDIR} -t ${NPROC} -m r941_min_high_g303 -b 50
 #${PORE}_${DEVICE}_${CALLER}_${CALLERVER}
 
 if [ $? -eq 0 ]; then
