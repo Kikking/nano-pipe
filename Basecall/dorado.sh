c4a1d5f0d23ee2fd033ebd0d800061cabd5cb969
Kikking <kikkingkeenan@gmail.com>
2023-10-17 21:48:51 +0200

    changed output to hardrive

--- Basecall/dorado.sh
+++ Basecall/dorado.sh
@@ -4,5 +4,5 @@ TARGET=${1-p5}
 MODEL=${2-dna_r9.4.1_e8_hac@v3.3}
 
 
- dorado basecaller $MODEL ~/darter/p5/${TARGET}.pod5 --emit-fastq > ~/darter/fq/${TARGET}.fastq
+ dorado basecaller $MODEL ~/darter/p5/${TARGET}.pod5 --emit-fastq > ~/mnt/d/SGNEX/fq/${TARGET}.fastq
  
