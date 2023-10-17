2eb8b1efe899c8845669e4a0f370170b8e01b7c9
Kikking <kikkingkeenan@gmail.com>
2023-10-17 21:34:44 +0200

    rm loop

--- Basecall/dorado.sh
+++ Basecall/dorado.sh
@@ -3,7 +3,6 @@
 TARGET=${1-p5}
 MODEL=${2-dna_r9.4.1_e8_hac@v3.3}
 
-for POD in $TARGET/*
- do
- dorado basecaller $MODEL $POD --emit-fastq > fq/${POD%.*}.fastq
- done
+
+ dorado basecaller $MODEL ~/darter/p5/${TARGET}.pod5 --emit-fastq > ~/darter/fq/${TARGET}.fastq
+ 
