ccfab60589015aafff7dbf3f789e1932c8b530b1
Kikking <kikkingkeenan@gmail.com>
2023-10-17 15:55:44 +0200

    no loop

--- Basecall/untarscript.sh
+++ Basecall/untarscript.sh
@@ -5,14 +5,13 @@ NAME=$2
 #PLEX=${2-plex1} #plex1 or plex2
 #This takes .tar.gz files and extracts them into fast5 files
 
-for FILE in $TARGET/*; #tar106_data
-do 
+#for FILE in $TARGET/*; #tar106_data
 echo "******************************************************************"
 echo $FILE;
 echo "******************************************************************"
 time tar -xvf $FILE -C /mnt/c/Users/User/Desktop/darter/f5_untar/${NAME}; 
 echo "<<<<<<<<<<<<<<<<<<<DONE>>>>>>>>>>>>>>>>>>>>>>>>"
 #find f5_untar/${PLEX} -type f -iname  *"SGNEX"* -exec cp {} f5/${PLEX} \;
-done 
+
 
 #find f5_untar/plex2 -type f -iname  *"SGNEX"* -exec cp {} f5/plex2 \;
