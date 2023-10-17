4522d08627667737c258f81df07ff8ef342654da
Kikking <kikkingkeenan@gmail.com>
2023-10-17 16:41:29 +0200

    fixed file path

--- Basecall/pod5Converter.sh
+++ Basecall/pod5Converter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 NAME=$1
 
-
- single_to_multi_fast5 --input_path /mnt/c/Users/User/Desktop/darter/untar_f5/${NAME} --save_path /mnt/c/Users/User/Desktop/darter/multif5/${NAME} --filename_base batch_output --batch_size 400 --recursive
+echo "merging file path: /mnt/c/Users/User/Desktop/darter/f5_untar/"${NAME}
+ single_to_multi_fast5 --input_path /mnt/c/Users/User/Desktop/darter/f5_untar/${NAME} --save_path /mnt/c/Users/User/Desktop/darter/multif5/${NAME} --filename_base batch_output --batch_size 400 --recursive
  #Convert the multireads into pod5 format
-
+echo "::::::::::::::CONVERTING:::::::::::::"
  pod5 convert fast5 /mnt/c/Users/User/Desktop/darter/multif5/${NAME} --output p5/${NAME%.*}.pod5
  
