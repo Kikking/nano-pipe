ec26a97ef8cd029448d98bd5189d51491e0386da
Kikking <kikkingkeenan@gmail.com>
2023-10-17 08:29:01 +0200

    included multiplexing

--- Basecall/untarscript.sh
+++ Basecall/untarscript.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 TARGET=$1
+PLEX=${2-plex1} #plex1 or plex2
 #This takes .tar.gz files and extracts them into fast5 files
 
 for FILE in $TARGET/*; #tar106_data
 do 
+echo "******************************************************************"
 echo $FILE;
-tar -xvf $FILE -C f5_untar; 
-find f5_untar -type f -iname  *"SGNEX"* -exec cp {} f5/ \;
+echo "******************************************************************"
+tar -xvf $FILE -C f5_untar/${PLEX}; 
+echo "<<<<<<<<<<<<<<<<<<<FINDING>>>>>>>>>>>>>>>>>>>>>>>>"
+find f5_untar -type f -iname  *"SGNEX"* -exec cp {} f5/${PLEX} \;
 done 
 
 
