d2c56d9e2db257323f60628662bc0c685a861dca
Kikking <kikkingkeenan@gmail.com>
2023-10-17 12:12:25 +0200

    f5_untar

--- Basecall/pod5Converter.sh
+++ Basecall/pod5Converter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-TARGET=${1-f5}
+PLEX=${1-plex1}
 
-for FILE in $TARGET/*
+for FILE in f5_untar/${PLEX}/*
 do
- single_to_multi_fast5 --input_path $FILE --save_path multif5/${FILE##*/} --filename_base batch_output --batch_size 100 --recursive
+ single_to_multi_fast5 --input_path $FILE --save_path multif5/${PLEX}/${FILE##*/} --filename_base batch_output --batch_size 100 --recursive
  #Convert the multireads into pod5 format
  done
- for DIR in multif5/*
+ for DIR in multif5/${PLEX}/*
  pod5 convert fast5 $DIR --output p5/${DIR%.*}.pod5
  done
