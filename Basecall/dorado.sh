5911c9ee8a4be602787e6f6816447ff9c2044fd8
Kikking <kikkingkeenan@gmail.com>
2023-10-19 14:08:22 +0200

    included qc

--- Basecall/dorado.sh
+++ Basecall/dorado.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-TARGET=${1-p5}
+NAME=${1}
 MODEL=${2-dna_r9.4.1_e8_hac@v3.3}
 
+#basecall the pod5
+dorado basecaller $MODEL ~/darter/p5/${NAME}.pod5 --emit-fastq > /mnt/d/SGNEX/fq/${NAME}.fastq
 
- dorado basecaller $MODEL ~/darter/p5/${TARGET}.pod5 --emit-fastq > /mnt/d/SGNEX/fq/${TARGET}.fastq
+#QC the resulting fq
+mkdir /mnt/d/SGNEX/nplot/${NAME} #make dir for output
+pip install NanoPlot --upgrade append 2>/dev/null #Check np needs updgrade
+NanoPlot -v #check np version
+
+#NANOPLOT
+echo "XXXXXXX NANOPLOTTING XXXXXXXXXXX"
+time NanoPlot -t 2 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
+    
  
