6714eb24457f22a2f915cece49069fd2fddab71a
Kikking <kikkingkeenan@gmail.com>
2023-10-19 11:12:45 +0200

    included .fastq

--- QC/nanoplot.sh
+++ QC/nanoplot.sh
@@ -3,5 +3,5 @@ NAME=$1
 
 mkdir /mnt/d/SGNEX/nplot/${NAME}
 pip install NanoPlot --upgrade
-NanoPlot -t 6 --fastq ~/mnt/d/SGNEX/fq/${NAME}  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
+NanoPlot -t 6 --fastq ~/mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
     
