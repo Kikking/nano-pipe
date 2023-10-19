1d6915a0b32bbe33b59e6e83a14bfe8ebd269fec
Kikking <kikkingkeenan@gmail.com>
2023-10-19 11:20:39 +0200

    silenced output of update and included version num

--- QC/nanoplot.sh
+++ QC/nanoplot.sh
@@ -2,6 +2,8 @@
 NAME=$1
 
 mkdir /mnt/d/SGNEX/nplot/${NAME}
-pip install NanoPlot --upgrade
-NanoPlot -t 6 --fastq ~/mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
+pip install NanoPlot --upgrade append 2>/dev/null 
+NanoPlot -v
+echo "XXXXXXX NANOPLOTTING XXXXXXXXXXX"
+time NanoPlot -t 6 --fastq ~/mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
     
