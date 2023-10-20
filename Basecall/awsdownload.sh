e11d91b9c2a0bc77dc6826ed6e10b8f201126907
Kikking <kikkingkeenan@gmail.com>
2023-10-20 10:08:17 +0200

    added loop

--- Basecall/awsdownload.sh
+++ Basecall/awsdownload.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#SAMPLE=$1
 
-for arg in "$@"; do
-  echo "Executing looping on '$arg'"
+for SAMPLE in "$@"; do
+echo "::FETCHING::> '$SAMPLE'"
+aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE /mnt/d/gcp/tardata 
 done
-#aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE /mnt/d/gcp/tardata
