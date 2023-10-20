a269149b77a1c8f3d221c8c3530f593f24ba935b
Kikking <kikkingkeenan@gmail.com>
2023-10-20 10:05:41 +0200

    testing

--- Basecall/awsdownload.sh
+++ Basecall/awsdownload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SAMPLE_LIST=$1
+#SAMPLE=$1
 
-for SAMPLE in $SAMPLE_LIST
-do
-aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE /mnt/d/gcp/tardata
+for arg in "$@"; do
+  echo "Executing looping on '$arg'"
 done
+#aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE /mnt/d/gcp/tardata
