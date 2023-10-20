85e965b6b959f218a4c4ce0917e1f6c372893576
Kikking <kikkingkeenan@gmail.com>
2023-10-20 08:29:01 +0200

    added .sam

--- Mapping/samscript.sh
+++ Mapping/samscript.sh
@@ -3,5 +3,5 @@
 NAME=$1
 #MODE={$2-both} #sam, quali or both 
 
-samtools view -b /mnt/d/SGNEX/mini/$NAME | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.bam
+samtools view -b /mnt/d/SGNEX/mini/$NAME.sam | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.bam
 qualimap bamqc -bam /mnt/d/SGNEX/mini_bam/${NAME}.bam -outdir /mnt/d/SGNEX/qualimap/${NAME} -c -nw 400 -hm 3
