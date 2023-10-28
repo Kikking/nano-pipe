33c3eff355b1f21a5b4ed7953b29cc740e42308b
Kikking <kikkingkeenan@gmail.com>
2023-10-28 14:44:58 +0200

    repeat

--- Basecall/podtest.sh
+++ Basecall/podtest.sh
@@ -2,9 +2,9 @@
 
 NAME=$1
 
-    echo "$NAME is a MULTI file"
-    echo "::::::::::::::FINDING::::::::::::::::"
-    find ~/darter/f5_untar/${NAME} -type f -exec mv -t ~/darter/multif5/${NAME} {} +
-    echo "::::::::::::::CONVERTING:::::::::::::"
-    sleep 10
-    pod5 ~/darter/multif5/${NAME} --output /mnt/d/SGNEX/p5/${NAME}.pod5
+echo "$NAME is a MULTI file"
+echo "::::::::::::::FINDING::::::::::::::::"
+find ~/darter/f5_untar/${NAME} -type f -exec mv -t ~/darter/multif5/${NAME} {} +
+echo "::::::::::::::CONVERTING:::::::::::::"
+sleep 10
+pod5 ~/darter/multif5/${NAME} --output /mnt/d/SGNEX/p5/${NAME}.pod5
