3bfb8f03c2b11ea2d7cdb8fe44e5f33f5e86722a
Kikking <kikkingkeenan@gmail.com>
2023-10-28 14:48:19 +0200

    force overwrite

--- Basecall/podtest.sh
+++ Basecall/podtest.sh
@@ -7,4 +7,4 @@ echo "::::::::::::::FINDING::::::::::::::::"
 find ~/darter/f5_untar/${NAME} -type f -exec mv -t ~/darter/multif5/${NAME} {} +
 echo "::::::::::::::CONVERTING:::::::::::::"
 sleep 10
-pod5 convert fast5 ~/darter/multif5/${NAME} --output /mnt/d/SGNEX/p5/${NAME}.pod5
+pod5 convert fast5 ~/darter/multif5/${NAME} --output /mnt/d/SGNEX/p5/${NAME}.pod5 --force-overwrite
