b02bbef8723861f9247bed411748bb9143694e99
Kikking <kikkingkeenan@gmail.com>
2023-11-02 13:26:51 +0200

    mapping and qc

---
+++ Mapping/minisam.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.mmi
+SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf
+NAME=$1
+
+minimap2 -ax splice $SIRV_REF /mnt/d/SGNEX/fq/${NAME}.fastq | samtools view -b | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.bam
+
+qualimap bamqc -bam /mnt/d/SGNEX/mini_bam/${NAME}.bam -outdir /mnt/d/SGNEX/qualimap/${NAME} -c -nw 400 -hm 3
