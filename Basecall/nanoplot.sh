db2e6bb0885408a63c169bf4c0f86d0dd36f4364
Kikking <kikkingkeenan@gmail.com>
2023-11-03 17:09:25 +0200

    typo

--- Basecall/nanoplot.sh
+++ Basecall/nanoplot.sh
@@ -2,7 +2,7 @@
 
 
 
-for NAME in "$@"; do;
+for NAME in "$@"; do
 echo "XXXXXXX NANOPLOTTING" $NAME "XXXXXXXXXXX"
 time NanoPlot -t 4 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats --raw
 done
