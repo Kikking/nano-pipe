a5cd4f84ede6df63ebff1069393d4404fa7b1d98
Kikking <kikkingkeenan@gmail.com>
2023-11-03 17:30:31 +0200

    added multifile support

--- Basecall/dorado.sh
+++ Basecall/dorado.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-NAME=${1}
-MODEL=${2-dna_r9.4.1_e8_hac@v3.3}
 
+MODEL=${dna_r9.4.1_e8_hac@v3.3}
+
+pip install NanoPlot --upgrade append 2>/dev/null #Check np needs updgrade
+NanoPlot -v #check np version
+
+for NAME in "$@";do
 #basecall the pod5
 dorado basecaller $MODEL /mnt/d/SGNEX/p5/${NAME}.pod5 --emit-fastq > /mnt/d/SGNEX/fq/${NAME}.fastq
 
 #QC the resulting fq
 mkdir /mnt/d/SGNEX/nplot/${NAME} #make dir for output
-pip install NanoPlot --upgrade append 2>/dev/null #Check np needs updgrade
-NanoPlot -v #check np version
-
 #NANOPLOT
-echo "XXXXXXX NANOPLOTTING XXXXXXXXXXX"
-time NanoPlot -t 2 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
-    
+echo "XXXXXXX NANOPLOTTING" $NAME "XXXXXXXXXXX"
+time NanoPlot -t 6 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats --raw
+done 
  
