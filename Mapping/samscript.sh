6689499a1d16680262c6899680924c8b8455c62f
Kikking <kikkingkeenan@gmail.com>
2024-01-02 09:45:21 +0200

    reduced qualimap nw to 200

--- Mapping/samscript.sh
+++ Mapping/samscript.sh
@@ -4,4 +4,4 @@ NAME=$1
 #MODE={$2-both} #sam, quali or both 
 
 samtools view -b /mnt/d/SGNEX/mini/$NAME.sam | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.bam
-qualimap bamqc -bam /mnt/d/SGNEX/mini_bam/${NAME}.bam -outdir /mnt/d/SGNEX/qualimap/${NAME} -c -nw 400 -hm 3
+qualimap bamqc -bam /mnt/d/SGNEX/mini_bam/${NAME}.bam -outdir /mnt/d/SGNEX/qualimap/${NAME} -c -nw 200 -hm 3
