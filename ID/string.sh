2947a236c036974b872896ae7f4ae1dc4fae7a18
Kikking <kikkingkeenan@gmail.com>
2024-01-11 16:54:01 +0200

    moved var pos around

--- ID/string.sh
+++ ID/string.sh
@@ -6,4 +6,4 @@ NAME=$1
 SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 
-stringtie -G $SIRV_ANNO -o /mnt/d/SGNEX/String/${NAME}.gtf -L true -p 4 /mnt/d/SGNEX/mini_bam/${NAME}.bam
+stringtie -G $SIRV_ANNO -L true -p 4 -o /mnt/d/SGNEX/String/${NAME}.gtf  /mnt/d/SGNEX/mini_bam/${NAME}.bam
