a45305e13f727719caf414cc9aa1b127ea12f76d
Kikking <kikkingkeenan@gmail.com>
2024-01-11 17:05:07 +0200

    remove L = T and p param

--- ID/string.sh
+++ ID/string.sh
@@ -6,5 +6,5 @@ NAME=$1
 SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 echo $SIRV_ANNO
-stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -G $SIRV_ANNO -L true -p 4 -o /mnt/d/SGNEX/String/${NAME}.gtf  
+stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
 --_
