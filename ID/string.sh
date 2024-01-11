dfa96fd52539ff0d4936864f55fecea49fc8b7f4
Kikking <kikkingkeenan@gmail.com>
2024-01-11 16:44:08 +0200

    rm .gz

--- ID/string.sh
+++ ID/string.sh
@@ -3,7 +3,7 @@
 
 NAME=$1
 
-SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta.gz
-SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf.gz
+SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
+SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 
 stringtie -G $SIRV_ANNO -o /mnt/d/SGNEX/String/${NAME}.gtf -L true -p 4 /mnt/d/SGNEX/mini_bam/${NAME}.bam
