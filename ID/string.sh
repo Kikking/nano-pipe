61a01970df8252552cef948a15a10f87ae118fb8
Kikking <kikkingkeenan@gmail.com>
2024-01-11 16:57:55 +0200

    testing with echo

--- ID/string.sh
+++ ID/string.sh
@@ -5,5 +5,5 @@ NAME=$1
 
 SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
-
+echo /mnt/d/SGNEX/mini_bam/${NAME}.bam
 stringtie -G $SIRV_ANNO -L true -p 4 -o /mnt/d/SGNEX/String/${NAME}.gtf  /mnt/d/SGNEX/mini_bam/${NAME}.bam
