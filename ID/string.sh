27ca3e3524acdb9795b4b22c9fc8c321f1721d4f
Kikking <kikkingkeenan@gmail.com>
2024-01-11 16:42:50 +0200

    fixed ref location

--- ID/string.sh
+++ ID/string.sh
@@ -3,7 +3,7 @@
 
 NAME=$1
 
-SIRV_REF=~/darter/refData/lrgasp_grch38_sirvs.fasta.gz
-SIRV_ANNO=~/darter/refData/lrgasp_gencode_v38_sirvs.gtf.gz
+SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta.gz
+SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf.gz
 
 stringtie -G $SIRV_ANNO -o /mnt/d/SGNEX/String/${NAME}.gtf -L true -p 4 /mnt/d/SGNEX/mini_bam/${NAME}.bam
