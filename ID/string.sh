4ae39b03c7d02ad92329e9e21a57c727015409ce
Kikking <kikkingkeenan@gmail.com>
2024-01-12 14:11:06 +0200

    Added estimation param

--- ID/string.sh
+++ ID/string.sh
@@ -6,5 +6,4 @@ NAME=$1
 SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 echo $SIRV_ANNO
-stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
---_
+stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
