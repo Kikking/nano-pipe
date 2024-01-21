8d873bc59983fa9bc4d16a451fa4c3e803d562ee
Kikking <kikkingkeenan@gmail.com>
2024-01-21 20:30:53 +0200

    can run on many files

--- ID/string.sh
+++ ID/string.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-
-NAME=$1
-
 SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
-echo $SIRV_ANNO
-stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
+
+for NAME in "$@"; do
+echo "::STRINGING::> '$NAME'"
+time stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
+done
+
+
+
