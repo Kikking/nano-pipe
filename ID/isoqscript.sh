2bc376849e2177a986a4aa2e667d49ca4006a3b2
Kikking <kikkingkeenan@gmail.com>
2024-01-22 12:35:23 +0200

    included samtools index step

--- ID/isoqscript.sh
+++ ID/isoqscript.sh
@@ -4,8 +4,10 @@ SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
 SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 
 NAME=$1
-#samtools index $TARGET > $HOME/mount2/minidata_index/"${TARGET##*/}".bam
-time isoquant.py --reference $SIRV_REF  --genedb $SIRV_ANNO --complete_genedb --bam /mnt/d/SGNEX/mini_bam/${NAME}.bam  --data_type nanopore -o /mnt/d/SGNEX/isoq/${NAME}
+echo "indexing..."
+time samtools index /mnt/d/SGNEX/mini_bam/${NAME}.bam  > /mnt/d/SGNEX/mini_bam/index/${NAME}.bam
+echo "quanting..."
+time isoquant.py --reference $SIRV_REF  --genedb $SIRV_ANNO --complete_genedb --bam  /mnt/d/SGNEX/mini_bam/index/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/isoq/${NAME}
 
 
 
