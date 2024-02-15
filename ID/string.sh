b86b7221dcf75fc96bb116a97ee412cd81aa2c32
Kikking <kikkingkeenan@gmail.com>
2024-02-15 12:07:39 +0200

    testing

--- ID/string.sh
+++ ID/string.sh
@@ -5,7 +5,7 @@ SIRV_ANNO=/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 
 for NAME in "$@"; do
 echo "::STRINGING::> '$NAME'"
-time stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/${NAME}.gtf  
+time stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -B -G $SIRV_ANNO -L -o /mnt/d/SGNEX/String/1502/${NAME}.gtf  
 done
 
 
