fd38d54a84ed515d6f208d5a3e4d389414e19405
Kikking <kikkingkeenan@gmail.com>
2024-02-23 06:56:35 +0200

    init

---
+++ ID/tcleaner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.fasta
+NAME=$1 
+
+samtools view -s /mnt/d/SGNEX/mini_bam/${NAME}.bam | samtools sort > /mnt/d/SGNEX/talon_stuff/sam_files/${NAME}.sam
+transcriptclean --sam /mnt/d/SGNEX/talon_stuff/sam_files/${NAME}.sam --genome $SIRV_REF --outprefix /mnt/d/SGNEX/talon_stuff/clean_files
