62e76b7a7c3b8f67c8e31f7df0fd6fcf6558ed28
Kikking <kikkingkeenan@gmail.com>
2024-02-27 16:31:04 +0200

    disable_infer_transcripts=True

--- ID/isoqscript.sh
+++ ID/isoqscript.sh
@@ -7,7 +7,7 @@ NAME=$1
 echo "indexing..."
 time samtools index /mnt/d/SGNEX/mini_bam/${NAME}.bam 
 echo "quanting..."
-time isoquant.py --reference $SIRV_REF --prefix $NAME --genedb $SIRV_ANNO -t 8 --high_memory --bam  /mnt/d/SGNEX/mini_bam/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/GTF_files/isoquant/
+time isoquant.py --reference $SIRV_REF --prefix $NAME --genedb $SIRV_ANNO -t 8 --high_memory disable_infer_transcripts=True --bam  /mnt/d/SGNEX/mini_bam/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/GTF_files/isoquant/
 echo "renaming..." 
 time mv /mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.extended_annotation.gtf /mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.gtf 
 
