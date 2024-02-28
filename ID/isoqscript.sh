f008d2a66df35b65c4ef5bd3659e3889259a8efa
Kikking <kikkingkeenan@gmail.com>
2024-02-28 06:45:10 +0200

    testing without index

--- ID/isoqscript.sh
+++ ID/isoqscript.sh
@@ -5,7 +5,7 @@ SIRV_ANNO=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs_v5_reformatted.gtf
 
 NAME=$1
 echo "indexing..."
-time samtools index /mnt/d/SGNEX/mini_bam/${NAME}.bam 
+#time samtools index /mnt/d/SGNEX/mini_bam/${NAME}.bam 
 echo "quanting..."
 time isoquant.py --reference $SIRV_REF --prefix $NAME --genedb $SIRV_ANNO --complete_genedb --clean_start -t 8 --high_memory --bam  /mnt/d/SGNEX/mini_bam/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/GTF_files/isoquant/
 echo "renaming..." 
