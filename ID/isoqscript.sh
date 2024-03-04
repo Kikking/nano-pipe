5bb5acd2e6b50f78d7844ae4c13eeef96c0e92c6
Kikking <kikkingkeenan@gmail.com>
2024-03-04 22:34:24 +0200

    my_data anno

--- ID/isoqscript.sh
+++ ID/isoqscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SIRV_REF=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa 
-SIRV_ANNO="/mnt/d/refData/Rgtf2.db"
+SIRV_ANNO=/mnt/d/refData/my_data.db
 #/mnt/d/refData/lrgasp_gencode_v38_sirvs.gtf
 #/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs_v5_reformatted.gtf
 
@@ -9,7 +9,7 @@ NAME=$1
 echo "indexing..."
 #time samtools index /mnt/d/SGNEX/mini_bam/${NAME}.bam 
 echo "quanting..."
-time isoquant.py --reference $SIRV_REF --prefix $NAME --genedb $SIRV_ANNO --complete_genedb --clean_start -t 8 --high_memory --bam  /mnt/d/SGNEX/mini_bam/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/GTF_files/isoquant/
+time isoquant.py --force --reference $SIRV_REF --prefix $NAME --genedb $SIRV_ANNO --complete_genedb --clean_start -t 8 --high_memory --bam  /mnt/d/SGNEX/mini_bam/${NAME}.bam --data_type nanopore -o /mnt/d/SGNEX/GTF_files/isoquant/
 echo "renaming..." 
 time mv /mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.extended_annotation.gtf /mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.gtf 
 
