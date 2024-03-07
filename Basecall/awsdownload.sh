0669034a1b3105506d43672204fb6a145ae9bd14
Kikking <kikkingkeenan@gmail.com>
2024-03-07 21:36:31 +0200

    reverted changes

--- Basecall/awsdownload.sh
+++ Basecall/awsdownload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
+#without fa,gz 
 for SAMPLE in "$@"; do
 echo "::FETCHING::> '$SAMPLE'"
-time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE /mnt/d/gcp/tardata 
+time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE/ /mnt/d/gcp/tardata 
 done
