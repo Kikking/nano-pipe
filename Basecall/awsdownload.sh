cdb04ffae597863d87492883aa251083a7ecf949
Kikking <kikkingkeenan@gmail.com>
2024-03-09 06:40:03 +0200

    using wget -c for larger downloads

--- Basecall/awsdownload.sh
+++ Basecall/awsdownload.sh
@@ -3,5 +3,5 @@
 #without fa,gz 
 for SAMPLE in "$@"; do
 echo "::FETCHING::> '$SAMPLE'"
-time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_ont/fast5/$SAMPLE/ /mnt/d/gcp/tardata 
+time wget -c http://sg-nex-data.s3.amazonaws.com/data/sequencing_data_ont/fast5/$SAMPLE/$SAMPLE.tar.gz  /mnt/d/gcp/tardata 
 done
