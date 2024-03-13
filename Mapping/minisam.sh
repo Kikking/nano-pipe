985fa66e1d3ef3447e0ed50872f8384ca8621090
Kikking <kikkingkeenan@gmail.com>
2024-03-13 06:59:11 +0200

    using old ref

--- Mapping/minisam.sh
+++ Mapping/minisam.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-SIRV_REF=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa.mmi
+#SIRV_REF=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa.mmi
+SIRV_REF=/mnt/d/refData/lrgasp_grch38_sirvs.mmi
 SIRV_ANNO=$HOME/mount/ref_data/lrgasp_gencode_v38_sirvs.gtf
 NAME=$1
 
-minimap2 -ax splice $SIRV_REF -t 6 --MD /mnt/d/SGNEX/fq/${NAME}.fastq | samtools view -b | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.1.bam
+minimap2 -ax splice $SIRV_REF -t 6 --MD /mnt/d/SGNEX/fq/${NAME}.fastq | samtools view -b | samtools sort > /mnt/d/SGNEX/mini_bam/${NAME}.bam
 
