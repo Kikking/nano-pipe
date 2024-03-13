3a7dad4eb5f9303a09c12c1665a8cc04bdce3fad
Kikking <kikkingkeenan@gmail.com>
2024-03-13 21:50:09 +0200

    first init

---
+++ Mapping/STARindex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+STAR --runThreadN 8 \
+==runMode genomeGenerate \
+--genomeDir /mnt/e/refData/star_index/ \
+--genomeFastaFiles /mnt/e/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa \
+--sjdbGTFfile /mnt/e/refdata/hg38_sequins_SIRV_ERCCs_longSIRVs_v5_reformatted.gtf
