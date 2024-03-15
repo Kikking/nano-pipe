187392d909cc082210a982fb47c8fa2d0a888c9d
Kikking <kikkingkeenan@gmail.com>
2024-03-15 21:05:38 +0200

    updated anno

--- ID/sqantiscript.sh
+++ ID/sqantiscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SIRV_REF=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa 
-SIRV_ANNO=/mnt/d/refData/hg38_sequins_SIRV_ERCCs_longSIRVs_v5_reformatted.gtf
+SIRV_REF=/mnt/e/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa 
+SIRV_ANNO=/mnt/e/refData/SIRV_edited.gtf
 TARGET="$1"
 TOOL="$2" # b = bambu, s = stringtie , i = isoquant
 
