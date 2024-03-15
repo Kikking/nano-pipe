ff274f0d1f3de3233dd4cead3c6ea46e0fffd263
Kikking <kikkingkeenan@gmail.com>
2024-03-15 15:36:45 +0200

    rm -e tag and added new gtf

--- ID/string.sh
+++ ID/string.sh
@@ -3,11 +3,11 @@
 #SIRV_ANNO=/mnt/e/refData/lrgasp_gencode_v38_sirvs.gtf
 #SIRV_REF=/mnt/e/refData/lrgasp_grch38_sirvs.fasta
 SIRV_REF=/mnt/e/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa 
-SIRV_ANNO=/mnt/e/refData/output_test1.gtf
+SIRV_ANNO=/mnt/e/refData/SIRV_edited.gtf
 
 for NAME in "$@"; do
 echo "::STRINGING::> '$NAME'"
-time stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -e -B -G $SIRV_ANNO -L -o /mnt/d/SGNEX/GTF_files/stringtie/${NAME}/${NAME}.gtf  
+time stringtie /mnt/d/SGNEX/mini_bam/${NAME}.bam -B -G $SIRV_ANNO -L -o /mnt/d/SGNEX/GTF_files/stringtie/${NAME}/${NAME}.gtf  
 done
 
 
