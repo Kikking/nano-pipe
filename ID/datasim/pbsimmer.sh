7a65cb75c6ffe272f0fb4d10be1014f3da0a5fea
Kikking <kikkingkeenan@gmail.com>
2024-03-17 21:03:04 +0200

    init commit

---
+++ ID/datasim/pbsimmer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+COUNT=${1:-}
+LENGTH=${2:-9000}
+LENGTHSD=${3:-7000}
+ACCURACY=${4:-0.85}
+
+awk 'BEGIN {OFS="\t"} {$2=$2"$COUNT";$3=$3"$COUNT"}1' sample.transcript > ~/pbsim3-3.0.4/sample/sample.transcript_$COUNT
+
+ pbsim --strategy trans --method errhmm \
+ --errhmm ~/pbsim3-3.0.4/data/ERRHMM-RSII.model \
+ --transcript ~/pbsim3-3.0.4/sample/sample.transcript_$COUNT \
+ --prefix /mnt/d/SGNEX/fq/sd_$COUNT_$LENGTH-$LENGTHSD_$ACCURACY \
+   --length-mean $LENGTH \
+  --length-sd  $LENGTHSD \
+  --accuracy-mean $ACCURACY 
