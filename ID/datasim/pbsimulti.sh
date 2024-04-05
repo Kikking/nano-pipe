cb1608cb512d5e9d0ff58994cfd020599546e5fb
Kikking <kikkingkeenan@gmail.com>
2024-04-05 08:51:26 +0200

    retries on failure, adds colour to messages

--- ID/datasim/pbsimulti.sh
+++ ID/datasim/pbsimulti.sh
@@ -1,46 +1,82 @@
-#!/bin/bash 
+#!/bin/bash
 
+# Define colors
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
+# Initialize variables
 COUNT=10
 LENGTH=9000
 LENGTHSD=2000
 ACCURACY=0.85
 ID=0
 
-while getopts L:A:C:S:ID: flag
-do
+# Parse command-line arguments
+while getopts "L:A:C:S:ID:" flag; do
     case "${flag}" in
-        L) LENGTH=${OPTARG};;
-        A) ACCURACY=${OPTARG};;
-        C) COUNT=${OPTARG};;
-        S) SAMPLE_DIR=${OPTARG};;
-        ID) ID=${OPTARG};;
+        L) LENGTH=${OPTARG} ;;
+        A) ACCURACY=${OPTARG} ;;
+        C) COUNT=${OPTARG} ;;
+        S) SAMPLE_DIR=${OPTARG} ;;
+        ID) ID=${OPTARG} ;;
     esac
 done
 
+# Function to run pbsim command with retry logic
+run_pbsim() {
+    local sample_file="$1"
+    local id="$2"
 
+    # Retry parameters
+    local max_retries=3
+    local retry=0
 
-for SAMPLE_FILE in $(ls ~/pbsim3-3.0.4/sample/$SAMPLE_DIR/*); do
-ID=$((ID+1))
+    while [ "$retry" -lt "$max_retries" ]; do
+        if pbsim --strategy trans --method errhmm \
+            --errhmm ~/pbsim3-3.0.4/data/ERRHMM-RSII.model \
+            --transcript "${sample_file}_${COUNT}" \
+            --prefix "/mnt/e/pbsimulti/sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}_${id}"  \
+            --length-mean "$LENGTH" \
+            --length-sd "$LENGTHSD" \
+            --accuracy-mean "$ACCURACY"
+        then
+            echo "${GREEN}PBSIM command successful${RESET}"
+            return 0
+        else
+            echo "${RED}PBSIM command failed with segmentation fault${RESET}"
+            echo "${BLUE}Retrying...${RESET}"
+            ((retry++))
+        fi
+    done
 
-awk -v count="$COUNT" 'BEGIN {OFS="\t"} {$2=($2*count);$3=($3*count)}1' ${SAMPLE_FILE} > ${SAMPLE_FILE}_"$COUNT"
+    echo "${RED}PBSIM command failed after $max_retries retries${RESET}"
+    return 1
+}
 
- pbsim --strategy trans --method errhmm \
- --errhmm ~/pbsim3-3.0.4/data/ERRHMM-RSII.model \
- --transcript ${SAMPLE_FILE}_$COUNT \
- --prefix "/mnt/e/pbsimulti/sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}_${ID}"  \
- --length-mean $LENGTH \
- --length-sd $LENGTHSD \
- --accuracy-mean $ACCURACY
+# Main loop to process sample files
+for SAMPLE_FILE in ~/pbsim3-3.0.4/sample/"$SAMPLE_DIR"/*; do
+    ID=$((ID+1))
 
- echo REMOVING ${SAMPLE_FILE}_"$COUNT"
- rm ${SAMPLE_FILE}_"$COUNT"
- done
+    # Run pbsim command with retry logic
+    if ! run_pbsim "$SAMPLE_FILE" "$ID"; then
+        echo "${RED}Error: PBSIM command failed for $SAMPLE_FILE${RESET}"
+        exit 1
+    fi
 
-tput setaf 6
-echo ":::: MERGINg ::::"
+    # Clean up temporary files
+    echo "${BLUE}Removing ${SAMPLE_FILE}_${COUNT}${RESET}"
+    rm "${SAMPLE_FILE}_${COUNT}"
+done
+
+# Merge fastq files
+echo "${BLUE}:::: Merging ::::${RESET}"
 find /mnt/e/pbsimulti/ -type f -name "*.fastq" -exec cat {} + > /mnt/d/SGNEX/fq/sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}_M.fastq
-echo ":::: CLEANING ::::"
-rm /mnt/e/pbsimulti/* 
-tput setaf 8
-echo "KEY: sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}_M" 
-tput sgr0
+
+# Clean up temporary directory
+echo "${BLUE}:::: Cleaning ::::${RESET}"
+rm /mnt/e/pbsimulti/*
+
+# Print key
+echo "${GREEN}Key: sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}_M${RESET}"
