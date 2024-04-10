962b857f212dc7bffb7aacfa53d54ec6483e2550
Kikking <kikkingkeenan@gmail.com>
2024-04-10 11:00:27 +0200

    fixed typo

--- Mapping/STARindex.sh
+++ Mapping/STARindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SIRV_REF=/mnt/e/refData/current/GRCh38.p14_chr1S_SIRV.fa
-SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtff
+SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf
 
 STAR --runThreadN 8 \
 --runMode genomeGenerate \
