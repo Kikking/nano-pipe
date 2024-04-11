4d834fdc60c191c96103dec1e45539a580278026
Kikking <kikkingkeenan@gmail.com>
2024-04-11 16:16:31 +0200

    re p4

--- Mapping/STARalign.sh
+++ Mapping/STARalign.sh
@@ -2,8 +2,8 @@
 
 # Define variables
 INDEX="/mnt/e/refData/STAR_index"
-READ1="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate3_run1/SGNex_Hct116_Illumina_replicate3_run1_R1_val_1.fq"
-READ2="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate3_run1/SGNex_Hct116_Illumina_replicate3_run1_R2_val_2.fq"
+READ1="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate4_run1/SGNex_Hct116_Illumina_replicate4_run1_R1_val_1.fq"
+READ2="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate4_run1/SGNex_Hct116_Illumina_replicate4_run1_R2_val_2.fq"
 
 # Calculate genomeChrBinNbits value (replace this with a constant integer)
 #GENOME_CHR_BIN_NBITS=18
@@ -14,6 +14,6 @@ STAR --runMode alignReads \
      --readFilesIn "$READ1" "$READ2" \
      --runThreadN 6 \
      --genomeLoad LoadAndKeep \
-    # --genomeChrBinNbits "$GENOME_CHR_BIN_NBITS" \
+     --genomeChrBinNbits "$GENOME_CHR_BIN_NBITS" \
      --genomeSAindexNbases 10 \
      --genomeSAsparseD 2
