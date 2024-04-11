83f5d90a15fbfc969dc88b975b659dabc9771c4a
Kikking <kikkingkeenan@gmail.com>
2024-04-11 07:59:57 +0200

    remove space line 7

--- Mapping/STARalign.sh
+++ Mapping/STARalign.sh
@@ -4,7 +4,7 @@ INDEX=/mnt/e/refData/STAR_index/SAindex
 READ1=/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate3_run1/SGNex_Hct116_Illumina_replicate3_run1_R1_val_1.fq.gz
 READ2=/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate3_run1/SGNex_Hct116_Illumina_replicate3_run1_R2_val_2.fq.gz
 
-STAR  alignReads --genomeDir $INDEX  \
+STAR alignReads --genomeDir $INDEX  \
  --readFilesIn $READ1 $READ2 \
  --runThreadN 6 \
  --genomeLoad LoadAndKeep \
