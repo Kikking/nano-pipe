62ed60ae5246fdb848edfaa2880476013474ed6a
Kikking <kikkingkeenan@gmail.com>
2024-04-12 07:24:08 +0200

    re p5

--- Mapping/STARalign.sh
+++ Mapping/STARalign.sh
@@ -2,8 +2,8 @@
 
 # Define variables
 INDEX="/mnt/e/refData/STAR_index"
-READ1="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate5_run1/SGNex_Hct116_Illumina_replicate4_run1_R1_val_1.fq"
-READ2="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate5_run1/SGNex_Hct116_Illumina_replicate4_run1_R2_val_2.fq"
+READ1="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate5_run1/SGNex_Hct116_Illumina_replicate5_run1_R1_val_1.fq"
+READ2="/mnt/d/barbet/trim/SGNex_Hct116_Illumina_replicate5_run1/SGNex_Hct116_Illumina_replicate5_run1_R2_val_2.fq"
 
 # Calculate genomeChrBinNbits value (replace this with a constant integer)
 GENOME_CHR_BIN_NBITS=18
