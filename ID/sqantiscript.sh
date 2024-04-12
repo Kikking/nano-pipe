0f03546ffbe2b2fd2aca26e4eb1c2f7fc5535186
Kikking <kikkingkeenan@gmail.com>
2024-04-12 11:33:19 +0200

    added more file pathing fixes

--- ID/sqantiscript.sh
+++ ID/sqantiscript.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
 SIRV_REF=/mnt/e/refData/current/GRCh38.p14_chr1S_SIRV.fa
-SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf
+SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.db
 TARGET="$1"
 TOOL="$2" # b = bambu, s = stringtie , i = isoquant
 
-if [[ "$TOOL" =~ ^[ibs]$ ]]; then
-  case "$TOOL" in
-    b) INDIR=bambu/${TARGET}/extended_annotations.gtf ;;
-    s) INDIR=stringtie/${TARGET}/${TARGET}.gtf ;;
-    i) INDIR=isoquant/${TARGET}/${TARGET}.gtf ;;
-  esac
-else
-  echo "Error: Invalid tool value '$TOOL'. Please use 'b', 's', or 'i'."
-  exit 1
+case "$TOOL" in
+  b)
+    INDIR=bambu/${TARGET}/extended_annotations.gtf
+    KEY=bambu
+    ;;
+  s)
+    INDIR=stringtie/${TARGET}/${TARGET}.gtf
+    KEY=stringtie
+    ;;
+  i)
+    INDIR=isoquant/${TARGET}/${TARGET}.gtf
+    KEY=isoquant
+    ;;
+  f)
+    INDIR=flair/${TARGET}/${TARGET}.gtf
+    KEY=flair
+    ;;
+
+  *)
+    echo "Error: Invalid tool value '$TOOL'."
+    exit 1
+    ;;
+esac
+
+if [[ "$TOOL" == "s" ]]; then
+    # Only perform awk command if tool is stringtie
+    awk -F'\t' 'BEGIN {OFS="\t"} {if ($7 == ".") $7 = "+"} 1' "$INDIR" > tmp.gtf
+    mv tmp.gtf ${INDIR}
 fi
 
- 
-#Run Sqanti script
-python sqanti3_qc.py \
-/mnt/d/SGNEX/GTF_files/"$INDIR" $SIRV_ANNO $SIRV_REF \
--d /mnt/d/SGNEX/sqantout/"$INDIR"/"$TARGET" \
---polyA_motif_list ~/SQANTI3/data/polyA_motifs/mouse_and_human.polyA_motif.txt \
---CAGE_peak ~/SQANTI3/data/ref_TSS_annotation/human.refTSS_v3.1.hg38.bed \
---skipORF \
---cpus 10 \
---report pdf                   
-
+# Run Sqanti script
+python ~/SQANTI3/sqanti3_qc.py \
+  "/mnt/d/SGNEX/GTF_files/$INDIR" "$SIRV_ANNO" "$SIRV_REF" \
+  -d "/mnt/d/SGNEX/sqantout/${KEY}/${TARGET}/" \
+  --polyA_motif_list ~/SQANTI3/data/polyA_motifs/mouse_and_human.polyA_motif.txt \
+  --CAGE_peak ~/SQANTI3/data/ref_TSS_annotation/human.refTSS_v3.1.hg38.bed \
+  --skipORF \
+  --cpus 10 \
+  --report pdf
 
 
 
