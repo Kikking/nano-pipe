deb20955d70053d164e79e559755547735765725
Kikking <kikkingkeenan@gmail.com>
2024-04-12 11:39:12 +0200

    typo

--- ID/flairscript.sh
+++ ID/flairscript.sh
@@ -2,7 +2,7 @@
 
 SIRV_REF=/mnt/e/refData/current/GRCh38.p14_chr1S_SIRV.fa
 SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf
-$TARGET=$1
+TARGET=$1
 
 echo ":::PUTTING $TARGET TO BED:::::>"
 bam2Bed12 -i /mnt/d/SGNEX/mini_bam/${TARGET}.bam > /mnt/e/flair_realm/sd_1_9000-2000_0.85.bed12
@@ -14,3 +14,15 @@ flair correct -q /mnt/e/flair_realm/${TARGET}.bed12 \
  --threads 8 \
  --print_check
 
+echo ":::BED IS COLLAPSING!!:::::>"
+flair collapse -q reads.flair_all_corrected.bed \
+-r $TARGET.fastq \
+-g $SIRV_REF --gtf $SIRV_ANNO \ 
+--stringent \
+--check_splice \
+--generate_map \
+--annotation_reliant generate \
+--support 1
+
+echo ":::QUANTIFYING:::::>"
+flair quantify -r reads_manifest -i flair.collapse.isoforms.fa
