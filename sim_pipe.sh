edc2ad372557f49aa7fda339c2e9efc81275bbaa
Kikking <kikkingkeenan@gmail.com>
2024-04-16 14:42:51 +0200

    added env activation and three more tools

--- sim_pipe.sh
+++ sim_pipe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ID_LOG="ALL_ID.log"
 MAP_LOG="MAPPING.log"
 
@@ -7,140 +8,158 @@ LENGTH=9000
 LENGTHSD=2000
 ACCURACY=0.85
 
-while getopts P:S: flag
-do
-    case "${flag}" in
-        P) PARAMETER_FILE=${OPTARG};;
-        S) SAMPLE_FILE=${OPTARG};;
-    esac
-done
+# Define Conda environments for each tool
+ENV_FLAIR="flair"
+ENV_TALON="talon"
+ENV_ESPRESSO="ESPRESSO"
+ENV_ISOQUANT="isoquant"
 
-dos2unix ${PARAMETER_FILE}
+# Function to activate Conda environment
+activate_env() {
+    local env_name="$1"
+    source activate "$env_name"
+}
+
+# Function to log messages with color
+log_message() {
+    local message="$1"
+    local color="$2"
+    echo -e "${color}${message}$(tput sgr0)"
+}
 
 # Function to perform ID tasks
 ALL_ID() {
-    # Define the file path
-    filepath="/mnt/d/SGNEX/mini_bam/${NAME}.bam"
+    local NAME="sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}"
+    local BAM_FILE="/mnt/d/SGNEX/mini_bam/${NAME}.bam"
 
-    # Check if the file exists
-    if [ -f "$filepath" ]; then
-    tput setaf 2 
-        echo "File ${NAME}.bam exists. Proceeding..."
-    tput sgr0
+    if [ -f "$BAM_FILE" ]; then
+        log_message "File ${NAME}.bam exists. Proceeding..." "$(tput setaf 2)"
+
         # STRINGTIE
-        STRING_PATH=/mnt/d/SGNEX/GTF_files/stringtie/${NAME}/${NAME}.gtf
-        if [ -f "$STRING_PATH" ]; then
-            tput setaf 3 
-            echo "STRING file ${NAME}.gtf exists. Skipping..."
-            tput sgr0
-        else
+        STRING_PATH="/mnt/d/SGNEX/GTF_files/stringtie/${NAME}/${NAME}.gtf"
+        if [ ! -f "$STRING_PATH" ]; then
+            log_message "Running STRINGTIE for $NAME..." "$(tput setaf 3)"
             time bash ~/nano-pipe/ID/string.sh "$NAME"
             bash ~/nano-pipe/template.sh "$NAME" STRINGTIE
+        else
+            log_message "STRING file ${NAME}.gtf exists. Skipping..." "$(tput setaf 3)"
         fi
+
         # ISOQUANT
-        ISOQUANT_PATH=/mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.gtf
-        if [ -f "$ISOQUANT_PATH" ]; then
-            tput setaf 3 
-            echo "isoQUANT file ${NAME}.gtf exists. Skipping..."
-            tput sgr0
-        else
-            time bash ~/nano-pipe/ID/isoqscript.sh "$NAME" 
+        ISOQUANT_PATH="/mnt/d/SGNEX/GTF_files/isoquant/${NAME}/${NAME}.gtf"
+        if [ ! -f "$ISOQUANT_PATH" ]; then
+            log_message "Running isoQUANT for $NAME..." "$(tput setaf 3)"
+            activate_env "$ENV_ISOQUANT"
+            time bash ~/nano-pipe/ID/isoqscript.sh "$NAME"
             bash ~/nano-pipe/template.sh "$NAME" ISOQUANT
-        fi
-        # BAMBU
-        BAMBU_PATH=/mnt/d/SGNEX/GTF_files/bambu/${NAME}/extended_annotations.gtf
-        if [ -f "$BAMBU_PATH" ]; then
-            tput setaf 3 
-            echo "BAMBU file ${NAME} exists. Skipping..."
-            tput sgr0
         else
+            log_message "isoQUANT file ${NAME}.gtf exists. Skipping..." "$(tput setaf 3)"
+        fi
+
+        # BAMBU
+        BAMBU_PATH="/mnt/d/SGNEX/GTF_files/bambu/${NAME}/extended_annotations.gtf"
+        if [ ! -f "$BAMBU_PATH" ]; then
+            log_message "Running BAMBU for $NAME..." "$(tput setaf 3)"
             time Rscript ~/nano-pipe/ID/bambush.R "$NAME"
             bash ~/nano-pipe/template.sh "$NAME" BAMBU
+        else
+            log_message "BAMBU file ${NAME} exists. Skipping..." "$(tput setaf 3)"
         fi
+
+        # TALON
+        TALON_PATH="/mnt/d/SGNEX/GTF_files/talon/${NAME}/${NAME}.gtf"
+        if [ ! -f "$TALON_PATH" ]; then
+            log_message "Running TALON for $NAME..." "$(tput setaf 3)"
+            activate_env "$ENV_TALON"
+            time bash ~/nano-pipe/ID/talonscript.sh "$NAME"
+        else
+            log_message "TALON output file exists. Skipping TALON..." "$(tput setaf 3)"
+        fi
+
+        # ESPRESSO
+        ESPRESSO_PATH="/mnt/d/SGNEX/GTF_files/espresso/${NAME}/${NAME}.gtf"
+        if [ ! -f "$ESPRESSO_PATH" ]; then
+            log_message "Running ESPRESSO for $NAME..." "$(tput setaf 3)"
+            activate_env "$ENV_ESPRESSO"
+            time bash ~/nano-pipe/ID/espressoscript.sh "$NAME"
+        else
+            log_message "Espresso output files exist. Skipping Espresso..." "$(tput setaf 3)"
+        fi
+
+        # FLAIR
+        FLAIR_PATH="/mnt/d/SGNEX/GTF_files/flair/${NAME}/${NAME}.gtf"
+        if [ ! -f "$FLAIR_PATH" ]; then
+            log_message "Running Flair for $NAME..." "$(tput setaf 3)"
+            activate_env "$ENV_FLAIR"
+            time bash ~/nano-pipe/ID/flairscript.sh "$NAME"
+        else
+            log_message "Flair output file exists. Skipping Flair..." "$(tput setaf 3)"
+        fi
+
     else
-    tput setaf 1
-        echo "File $filepath does not exist. Writing to log file $ID_LOG..."
-        echo "File $filepath does not exist." >> "$ID_LOG"
-    tput sgr0
+        log_message "File ${BAM_FILE} does not exist. Writing to log file ${ID_LOG}..." "$(tput setaf 1)"
+        echo "File ${BAM_FILE} does not exist." >> "$ID_LOG"
     fi
 }
 
 # Function to perform Mapping tasks
 MAPPING() {
-    # Define the file path
-    filepath="/mnt/d/SGNEX/fq/${NAME}.fastq"
+    local NAME="sd_${COUNT}_${LENGTH}-${LENGTHSD}_${ACCURACY}"
+    local FASTQ_FILE="/mnt/d/SGNEX/fq/${NAME}.fastq"
+    local MAP_PATH="/mnt/d/SGNEX/mini_bam/${NAME}.bam"
 
-    # Check if the file exists
-    if [ -f "$filepath" ]; then
-        tput setaf 2 
-        echo "File ${NAME}.fastq exists. Proceeding..."
-        tput sgr0
-        MAP_PATH="/mnt/d/SGNEX/mini_bam/${NAME}.bam"
-        if [ -f "$MAP_PATH" ]; then
-            tput setaf 3 
-            echo "MINIMAP2 file ${NAME} exists. Skipping..."
-            tput sgr0
-        else
+    if [ -f "$FASTQ_FILE" ]; then
+        log_message "File ${FASTQ_FILE} exists. Proceeding..." "$(tput setaf 2)"
+
+        if [ ! -f "$MAP_PATH" ]; then
+            log_message "Running MINIMAP2 for $NAME..." "$(tput setaf 3)"
             time bash ~/nano-pipe/Mapping/minisam.sh "$NAME"
+        else
+            log_message "MINIMAP2 file ${NAME} exists. Skipping..." "$(tput setaf 3)"
         fi
+
     else
-    tput setaf 1
-        echo "File $filepath does not exist. Writing to log file $MAP_LOG..."
-        echo "File $filepath does not exist." >> "$MAP_LOG"
-    tput sgr0
+        log_message "File ${FASTQ_FILE} does not exist. Writing to log file ${MAP_LOG}..." "$(tput setaf 1)"
+        echo "File ${FASTQ_FILE} does not exist." >> "$MAP_LOG"
     fi
 }
 
 # Read the CSV file line by line
-sed 1d ${PARAMETER_FILE} | while IFS=$'\t' read -r COUNT LENGTH LENGTHSD ACCURACY; do
+sed 1d "${PARAMETER_FILE}" | while IFS=$'\t' read -r COUNT LENGTH LENGTHSD ACCURACY; do
     # Process each field (value)
-    if [ $COUNT != 0 ]; then
-    tput setaf 5
-    echo "Count: $COUNT"
-    tput sgr0
-    bash ~/nano-pipe/ID/datasim/pbsimmer.sh -S ${SAMPLE_FILE} -C ${COUNT}
-    echo "------"
-    NAME=sd_${COUNT}_9000-2000_0.85
-    sleep 5
-    MAPPING
-    ALL_ID
+    if [ "$COUNT" -ne 0 ]; then
+        log_message "Count: $COUNT" "$(tput setaf 5)"
+        bash ~/nano-pipe/ID/datasim/pbsimmer.sh -S "${SAMPLE_FILE}" -C "${COUNT}"
+        echo "------"
+        NAME="sd_${COUNT}_9000-2000_0.85"
+        sleep 5
+        MAPPING
+        ALL_ID
     else
-    tput setaf 6
-    echo "SKIPPING"
-    tput sgr0
+        log_message "SKIPPING Count: $COUNT" "$(tput setaf 6)"
     fi
     
-    if [ $LENGTH != 0 ]; then
-    tput setaf 5
-    echo "Length: $LENGTH"
-    tput sgr0
-    bash ~/nano-pipe/ID/datasim/pbsimmer.sh -S ${SAMPLE_FILE} -L ${LENGTH}
-    echo "------"
-    NAME=sd_10_${LENGTH}-2000_0.85
-    sleep 5
-    MAPPING
-    ALL_ID
+    if [ "$LENGTH" -ne 0 ]; then
+        log_message "Length: $LENGTH" "$(tput setaf 5)"
+        bash ~/nano-pipe/ID/datasim/pbsimmer.sh -S "${SAMPLE_FILE}" -L "${LENGTH}"
+        echo "------"
+        NAME="sd_10_${LENGTH}-2000_0.85"
+        sleep 5
+        MAPPING
+        ALL_ID
     else
-    tput setaf 6
-    echo "SKIPPING"
-    tput sgr0
+        log_message "SKIPPING Length: $LENGTH" "$(tput setaf 6)"
     fi
 
-    if [ $ACCURACY != 0 ]; then
-    tput setaf 5
-    echo "Accuracy: $ACCURACY"
-    tput sgr0
-    bash ~/nano-pipe/ID/datasim/pbsimmer.sh -A ${ACCURACY} -S ${SAMPLE_FILE}
-    echo "------"
-    NAME=sd_10_9000-2000_${ACCURACY}
-    sleep 5
-    MAPPING
-    ALL_ID
+    if [ "$ACCURACY" != 0 ]; then
+        log_message "Accuracy: $ACCURACY" "$(tput setaf 5)"
+        bash ~/nano-pipe/ID/datasim/pbsimmer.sh -A "${ACCURACY}" -S "${SAMPLE_FILE}"
+        echo "------"
+        NAME="sd_10_9000-2000_${ACCURACY}"
+        sleep 5
+        MAPPING
+        ALL_ID
     else
-    tput setaf 6
-    echo "SKIPPING"
-    tput sgr0
+        log_message "SKIPPING Accuracy: $ACCURACY" "$(tput setaf 6)"
     fi
-   
-
 done
