314a66e861d8d37b39025727b1e14bcfb5764e2e
Kikking <kikkingkeenan@gmail.com>
2024-06-03 11:29:42 +0200

    uses gtf from c drive

--- ID/string.sh
+++ ID/string.sh
@@ -4,7 +4,8 @@
 #SIRV_REF=/mnt/e/refData/lrgasp_grch38_sirvs.fasta
 #SIRV_REF=/mnt/e/refData/hg38_sequins_SIRV_ERCCs_longSIRVs.fa 
 SIRV_REF=/mnt/e/refData/current/GRCh38.p14_chr1S_SIRV.fa
-SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf
+#SIRV_ANNO=/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf
+SIRV_ANNO=/home/kikking/long_realm/ref/gencode45_chrIS_SIRV.gtf
 #SIRV_ANNO=/mnt/e/refData/SIRV_edited.gtf
 
 for NAME in "$@"; do
