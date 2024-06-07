402a4d3954eb22b6241f106890751524665927ea
Kikking <kikkingkeenan@gmail.com>
2024-06-07 11:43:21 +0200

    fixed gtf file pathing

--- novel_stuff/gffcompare.sh
+++ novel_stuff/gffcompare.sh
@@ -6,7 +6,8 @@ stripped="${NAME#sd}"
 TAG="${stripped%%_*}"
 
 TRUE_GTF=/home/kikking/novel_realm/true/TRUE_${TAG}_gencode45_chrIS_SIRV.gtf
-NOVEL_GTF=/mnt/d/SGNEX/${TOOL}/novel/${NAME}/${NAME}.filt.gtf
+NOVEL_GTF="/mnt/d/SGNEX/GTF_files/${TOOL}/novel/${NAME}/${NAME}.filt.gtf"
+
 mkdir /mnt/d/SGNEX/gffcmp/${TOOL}/novel/${NAME}/
 ~/gffcompare/gffcompare -r $TRUE_GTF -o /mnt/d/SGNEX/gffcmp/${TOOL}/novel/${NAME}/ $NOVEL_GTF
 
--- novel_stuff/gtf_filter.sh
+++ novel_stuff/gtf_filter.sh
@@ -30,64 +30,64 @@ GFFCOMPARE() {
 # STRINGTIE
 STRING_PATH="/mnt/d/SGNEX/gffcmp/stringtie/novel/${NAME}/${NAME}.annotated.gtf"
 if [ ! -f "$STRING_PATH" ]; then
-    log_message "Filtering STRINGTIE $NAME..." "YELLOW"
+    log_message "Filtering STRINGTIE $NAME..." "$YELLOW"
     TOOL="stringtie"
     COUNT_FILE="/mnt/d/SGNEX/GTF_files/stringtie/novel/${NAME}/t_data.ctab"
     GTF_FILE="/mnt/d/SGNEX/GTF_files/stringtie/novel/${NAME}/${NAME}.gtf"
     awk '$11 > 0 {print $6}' "$COUNT_FILE" > "/mnt/d/SGNEX/GTF_files/stringtie/novel/${NAME}/${NAME}.trans_list"
     GFFCOMPARE "$NAME" "$TOOL"
 else
-    log_message "STRING file ${NAME}.annotated.gtf exists. Skipping..." "YELLOW"
+    log_message "STRING file ${NAME}.annotated.gtf exists. Skipping..." "$YELLOW"
 fi
 
 # ISOQUANT
 ISOQUANT_PATH="/mnt/d/SGNEX/gffcmp/isoquant/novel/${NAME}/${NAME}.annotated.gtf"
 if [ ! -f "$ISOQUANT_PATH" ]; then
-    log_message "Filtering isoQUANT $NAME..." "YELLOW"
+    log_message "Filtering isoQUANT $NAME..." "$YELLOW"
     TOOL="isoquant"
     COUNT_FILE="/mnt/d/SGNEX/GTF_files/isoquant/novel/${NAME}/${NAME}.transcript_tpm.tsv"
     GTF_FILE="/mnt/d/SGNEX/GTF_files/isoquant/novel/${NAME}/${NAME}.gtf"
     awk '$2 > 0 {print $1}' "$COUNT_FILE" > "/mnt/d/SGNEX/GTF_files/isoquant/novel/${NAME}/${NAME}.trans_list"
     GFFCOMPARE "$NAME" "$TOOL"
 else
-    log_message "isoQUANT file ${NAME}.annotated.gtf exists. Skipping..." "YELLOW"
+    log_message "isoQUANT file ${NAME}.annotated.gtf exists. Skipping..." "$YELLOW"
 fi
 
 # BAMBU
 BAMBU_PATH="/mnt/d/SGNEX/gffcmp/bambu/novel/${NAME}/${NAME}.annotated.gtf"
 if [ ! -f "$BAMBU_PATH" ]; then
-    log_message "Filtering BAMBU $NAME..." "YELLOW"
+    log_message "Filtering BAMBU $NAME..." "$YELLOW"
     TOOL="bambu"
     COUNT_FILE="/mnt/d/SGNEX/GTF_files/bambu/novel/${NAME}/counts_transcript.txt"
     GTF_FILE="/mnt/d/SGNEX/GTF_files/bambu/novel/${NAME}/extended_annotations.gtf"
     awk '$3 > 0 {print $1}' "$COUNT_FILE" > "/mnt/d/SGNEX/GTF_files/bambu/novel/${NAME}/${NAME}.trans_list"
     GFFCOMPARE "$NAME" "$TOOL"
 else
-    log_message "BAMBU file ${NAME}.annotated.gtf exists. Skipping..." "YELLOW"
+    log_message "BAMBU file ${NAME}.annotated.gtf exists. Skipping..." "$YELLOW"
 fi
 
 # TALON
 TALON_PATH="/mnt/d/SGNEX/gffcmp/talon/novel/${NAME}/${NAME}.annotated.gtf"
 if [ ! -f "$TALON_PATH" ]; then
-    log_message "Filtering TALON $NAME..." "YELLOW"
+    log_message "Filtering TALON $NAME..." "$YELLOW"
     TOOL="talon"
     COUNT_FILE="/mnt/d/SGNEX/GTF_files/talon/novel/${NAME}/${NAME}.tsv"
     GTF_FILE="/mnt/d/SGNEX/GTF_files/talon/novel/${NAME}/${NAME}.gtf"
     awk '$12 > 0 {print $4}' "$COUNT_FILE" > "/mnt/d/SGNEX/GTF_files/talon/novel/${NAME}/${NAME}.trans_list"
     GFFCOMPARE "$NAME" "$TOOL"
 else
-    log_message "TALON output file exists. Skipping TALON..." "YELLOW"
+    log_message "TALON output file exists. Skipping TALON..." "$YELLOW"
 fi
 
 # FLAIR
 FLAIR_PATH="/mnt/d/SGNEX/gffcmp/flair/novel/${NAME}/${NAME}.annotated.gtf"
 if [ ! -f "$FLAIR_PATH" ]; then
-    log_message "Filtering Flair $NAME..." "YELLOW"
+    log_message "Filtering Flair $NAME..." "$YELLOW"
     TOOL="flair"
     COUNT_FILE="/mnt/d/SGNEX/GTF_files/flair/novel/${NAME}/${NAME}.annotated_transcripts.alignment.counts"
     GTF_FILE="/mnt/d/SGNEX/GTF_files/flair/novel/${NAME}/${NAME}.gtf"
     awk '$2 > 0 {print $1}' "$COUNT_FILE" | sed 's/_ENSG[0-9]\+\.[0-9]\+//' > "/mnt/d/SGNEX/GTF_files/flair/novel/${NAME}/${NAME}.trans_list"
     GFFCOMPARE "$NAME" "$TOOL"
 else
-    log_message "Flair output file exists. Skipping Flair..." "YELLOW"
+    log_message "Flair output file exists. Skipping Flair..." "$YELLOW"
 fi
