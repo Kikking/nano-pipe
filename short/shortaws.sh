4978c06ecdfc410e0b8a53e8923b1abf70dc3361
Kikking <kikkingkeenan@gmail.com>
2024-06-11 15:32:37 +0200

    genome aligned bams

--- short/shortaws.sh
+++ short/shortaws.sh
@@ -3,7 +3,7 @@ INPUT=$1
 
 while read -r SAMPLE ; do
 echo "::FETCHING::> '$SAMPLE'"
-time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_illumina/bam/$SAMPLE/ /mnt/d/SGNEX/short_bam/
+time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_illumina/bam/genome/$SAMPLE/ /mnt/d/SGNEX/short_bam/
 done < $INPUT
 
 
