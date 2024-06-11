652e65641d5a6bfddef57003abbcd789e986efd8
Kikking <kikkingkeenan@gmail.com>
2024-06-11 15:29:33 +0200

    reads files

--- short/shortaws.sh
+++ short/shortaws.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+INPUT=$1
 
-for SAMPLE in "$@"; do
+while read -r SAMPLE ; do
 echo "::FETCHING::> '$SAMPLE'"
 time aws s3 sync --no-sign-request s3://sg-nex-data/data/sequencing_data_illumina/bam/$SAMPLE/ /mnt/d/SGNEX/short_bam/
-done
+done < $INPUT
+
+
