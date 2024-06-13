6ed1fb94974b63e99ca854e75c8b934dbe149d8e
Kikking <kikkingkeenan@gmail.com>
2024-06-13 19:28:12 +0200

    reads files

--- short/trimgalore.sh
+++ short/trimgalore.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
-NAME=$1
+INPUT_FILE=$1
+
+while read NAME ;do
 mkdir /mnt/e/barbet/trim/${NAME}
 trim_galore --cores 4 -o /mnt/e/barbet/trim/${NAME}/ --paired /mnt/d/SGNEX/short_fq/${NAME}_R1.fastq.gz /mnt/d/SGNEX/short_fq/${NAME}_R2.fastq.gz
+done < $INPUT_FILE
