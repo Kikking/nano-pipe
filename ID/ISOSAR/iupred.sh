a91619eb9a1e280c1c5d4f070e3e20875fd661e3
Kikking <kikkingkeenan@gmail.com>
2024-07-16 10:34:09 +0200

    processes one seq at a time

--- ID/ISOSAR/iupred.sh
+++ ID/ISOSAR/iupred.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 ID=$1
-mkdir /mnt/d/ISOSAR_files/${ID}/iupred
 
-python3 /mnt/d/ISOSAR_software/iupred2a/iupred2a.py -a -d /mnt/d/ISOSAR_software/iupred2a/ \
- /mnt/d/ISOSAR_files/${ID}/isoformSwitchAnalyzeR_isoform_AA.fasta \
- long > /mnt/d/ISOSAR_files/${ID}/iupred/iupredout.txt
+
+echo "splitting FASTA..."
+bash ~/nano-pipe/ID/ISOSAR/fasta_splitter.sh $ID
+
+# Directory containing split sequences
+split_dir=/mnt/d/ISOSAR_files/${ID}/fasta_split/
+# Directory to store individual outputs
+output_dir=/mnt/d/ISOSAR_files/${ID}/iupred/
+# Create the output directory
+mkdir -p "$output_dir"
+
+# Loop through each sequence file and run the script
+for seq_file in "$split_dir"/*.fasta; do
+    # Extract the base name of the file (without path and extension)
+    base_name=$(basename "$seq_file" .fasta)
+    python3 /mnt/d/ISOSAR_software/iupred2a/iupred2a.py -a \
+ -d /mnt/d/ISOSAR_software/iupred2a/ \
+ $seq_file \
+ long > $output_dir/$base_name.txt
+done
+
