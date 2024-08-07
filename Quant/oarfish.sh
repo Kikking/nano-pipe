968994852ec8a1053b17cd4ffc7b7332a601fe6b
Kikking <kikkingkeenan@gmail.com>
2024-08-07 09:03:54 +0200

    aligns to transcriptome prior

--- Quant/oarfish.sh
+++ Quant/oarfish.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-TARGET=$1
-OUTPUT=/mnt/d/SGNEX/quant/oarfish/${TARGET}/${TARGET}
+REF=~/long_realm/ref/transcriptome_chr1S_SIRV.fa #Transcriptome
+NAME=$1
+
+#Need to align to transcriptome prior
+minimap2 -ax splice $REF -t 8 --MD /mnt/d/SGNEX/fq/${NAME}.fastq | samtools view -b | samtools sort > /mnt/d/SGNEX/trans_bam/${NAME}.bam
+
+
+OUTPUT=/mnt/d/SGNEX/quant/oarfish/${NAME}/${NAME}
 mkdir -p ${OUTPUT}
 
 oarfish \
-    -a /mnt/d/SGNEX/mini_bam/${TARGET}.bam \
+    -a /mnt/d/SGNEX/trans_bam/${NAME}.bam \
     -o ${OUTPUT} \
     -j 10 #Threads
