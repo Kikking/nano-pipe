ca7e32afe56f7bffe5058b056ccea1b75d3c42b2
Kikking <kikkingkeenan@gmail.com>
2024-08-19 09:37:19 +0200

    UNIX pathing

--- ID/bambush.R
+++ ID/bambush.R
@@ -4,12 +4,12 @@ library(bambu)
   fa.file <- "//wsl.localhost/Ubuntu-22.04/home/kikking/long_realm/ref/GRCh38.p14_chr1S_SIRV.fa"
   #fa.file <- "E:/refData/lrgasp_grch38_sirvs.fasta"
   
-  gtf.file <- "E:/refData/current/gencode45_chrIS_SIRV.gtf"
+  gtf.file <- "/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf"
   #gtf.file <- "E:/refData/lrgasp_gencode_v38_sirvs.gtf"
   
   #annotations <- prepareAnnotations(gtf.file)
   #saveRDS(annotations, file = "E:/refData/current/gencode45_chrIS_SIRV_annotations.rds")
-  annotations <- readRDS("E:/refData/current/gencode45_chrIS_SIRV_annotations.rds")
+  annotations <- readRDS("/mnt/e/refData/current/gencode45_chrIS_SIRV_annotations.rds")
   #print(samples.bam <- list.files("D:/SGNEX/mini_bam", pattern = ".bam$", full.names = TRUE))
   
   ### EDIT HERE ###
@@ -17,9 +17,9 @@ library(bambu)
   
   #print(sampleData)
   #################
-    filepath <- paste0("D:/SGNEX/mini_bam/",sample,".bam") 
+    filepath <- paste0("/mnt/d/SGNEX/mini_bam/",sample,".bam") 
     print(filepath)
     se <- bambu(reads = filepath, annotations = annotations, genome = fa.file, lowMemory  = FALSE, quant = T, trackReads = FALSE, discovery = TRUE)
     #writeToGTF(se, paste0("D:/SGNEX/GTF_files/bambu", sub(".bam", "", sub("D:/SGNEX/mini_bam", "", filepath)),".gtf"))
-    writeBambuOutput(se, paste0("D:/SGNEX/GTF_files/bambu/",sub(".bam", "", sub("D:/SGNEX/mini_bam", "", sample))))
+    writeBambuOutput(se, paste0("/mnt/d/SGNEX/GTF_files/bambu/",sub(".bam", "", sub("/mnt/d/SGNEX/mini_bam", "", sample))))
     
