54a9a32a97493a315832cd60d38a61a8a3b612a6
Kikking <kikkingkeenan@gmail.com>
2024-08-19 09:40:08 +0200

    takes sample from UNIX

--- ID/bambush.R
+++ ID/bambush.R
@@ -1,25 +1,20 @@
 library(bambu)
 
+# Capture command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+sample <- args[1]
 
-  fa.file <- "//wsl.localhost/Ubuntu-22.04/home/kikking/long_realm/ref/GRCh38.p14_chr1S_SIRV.fa"
-  #fa.file <- "E:/refData/lrgasp_grch38_sirvs.fasta"
-  
-  gtf.file <- "/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf"
-  #gtf.file <- "E:/refData/lrgasp_gencode_v38_sirvs.gtf"
-  
-  #annotations <- prepareAnnotations(gtf.file)
-  #saveRDS(annotations, file = "E:/refData/current/gencode45_chrIS_SIRV_annotations.rds")
-  annotations <- readRDS("/mnt/e/refData/current/gencode45_chrIS_SIRV_annotations.rds")
-  #print(samples.bam <- list.files("D:/SGNEX/mini_bam", pattern = ".bam$", full.names = TRUE))
-  
-  ### EDIT HERE ###
-  #sampleData <- bam_list(x=c(9,10,11) )
-  
-  #print(sampleData)
-  #################
-    filepath <- paste0("/mnt/d/SGNEX/mini_bam/",sample,".bam") 
-    print(filepath)
-    se <- bambu(reads = filepath, annotations = annotations, genome = fa.file, lowMemory  = FALSE, quant = T, trackReads = FALSE, discovery = TRUE)
-    #writeToGTF(se, paste0("D:/SGNEX/GTF_files/bambu", sub(".bam", "", sub("D:/SGNEX/mini_bam", "", filepath)),".gtf"))
-    writeBambuOutput(se, paste0("/mnt/d/SGNEX/GTF_files/bambu/",sub(".bam", "", sub("/mnt/d/SGNEX/mini_bam", "", sample))))
-    
+fa.file <- "//wsl.localhost/Ubuntu-22.04/home/kikking/long_realm/ref/GRCh38.p14_chr1S_SIRV.fa"
+gtf.file <- "/mnt/e/refData/current/gencode45_chrIS_SIRV.gtf"
+
+annotations <- readRDS("/mnt/e/refData/current/gencode45_chrIS_SIRV_annotations.rds")
+
+# Create file path
+filepath <- paste0("/mnt/d/SGNEX/mini_bam/", sample, ".bam")
+print(filepath)
+
+# Run bambu
+se <- bambu(reads = filepath, annotations = annotations, genome = fa.file, lowMemory = FALSE, quant = TRUE, trackReads = FALSE, discovery = TRUE)
+
+# Write output
+writeBambuOutput(se, paste0("/mnt/d/SGNEX/GTF_files/bambu/", sub(".bam", "", sub("/mnt/d/SGNEX/mini_bam/", "", sample))))
