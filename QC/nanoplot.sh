cf984ce7d1fcffadbf59f86a6cb41b2baebc35cd
Kikking <kikkingkeenan@gmail.com>
2024-12-12 07:47:03 +0200

    added --raw and increased t

--- QC/nanoplot.sh
+++ QC/nanoplot.sh
@@ -5,5 +5,5 @@ mkdir /mnt/d/SGNEX/nplot/${NAME}
 pip install NanoPlot --upgrade append 2>/dev/null 
 NanoPlot -v
 echo "XXXXXXX NANOPLOTTING XXXXXXXXXXX"
-time NanoPlot -t 2 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats
+time NanoPlot -t 10 --tsv_stats --raw --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} 
     
