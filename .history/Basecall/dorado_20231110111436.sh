9cff2dc039905758b6a2ff008278b09f32f5470a
Kikking <kikkingkeenan@gmail.com>
2025-03-08 10:13:36 +0200

    rna calling

---
+++ .history/Basecall/dorado_20231110111436.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+MODEL=dna_r9.4.1_e8_hac@v3.3
+
+pip install NanoPlot --upgrade append 2>/dev/null #Check np needs updgrade
+NanoPlot -v #check np version
+
+for NAME in "$@";do
+#basecall the pod5
+dorado basecaller $MODEL /mnt/d/SGNEX/p5/${NAME}.pod5 --emit-fastq > /mnt/d/SGNEX/fq/${NAME}.fastq
+
+#QC the resulting fq
+mkdir /mnt/d/SGNEX/nplot/${NAME} #make dir for output
+#NANOPLOT
+echo "XXXXXXX NANOPLOTTING" $NAME "XXXXXXXXXXX"
+time NanoPlot -t 6 --fastq /mnt/d/SGNEX/fq/${NAME}.fastq  -o /mnt/d/SGNEX/nplot/${NAME} --tsv_stats --raw
+done 
+ 
